f32498ec877cf0a4dff0e72e869d957535251e71
tutor6 <tutor6@hphk.kr>
2022-07-25 21:50:46 +0900

    Update 20220726

---
+++ 1회차/강동현/20220726/6_오르막길.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/2846
+import sys
+
+sys.stdin = open("6_오르막길.txt")
---
+++ 2회차/이상용/20220726/5_분해합.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/2231
+import sys
+
+sys.stdin = open("5_분해합.txt")
