47211f1b85d13527808c4172c6cc658e984a787a
danny7128 <danny7128@gmail.com>
2022-07-25 15:31:30 +0900

    20220725 필수 문제 완료

--- 3회차/이성훈/20220725/0_숫자의개수.py
+++ 3회차/이성훈/20220725/0_숫자의개수.py
@@ -2,3 +2,25 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+# 딕셔너리 key값 0~9까지 만듬
+E = {'0' : 0, '1' : 0 ,'2' : 0, '3' : 0, '4' : 0, '5' : 0, '6' : 0, '7' : 0, '8' : 0, '9' : 0}
+# 변수 A, B, C에 상수 값 입력 
+A = int(input())
+B = int(input())
+C = int(input())
+
+# 변수 D에 곲한값 저장
+D = A*B*C
+# 상수 D를 문자로 변환
+str_D = str(D)
+# 문자 D를 쪼깸
+for i in str_D:
+    # 딕셔너리E에 i(숫자)가 있다면 1증가
+    if i in E:
+        E[i] += 1
+    else:
+        E[i] = 1
+# 0~9까지 Value값만 출력
+for i in E:
+    print(E[i])
+    
--- 3회차/이성훈/20220725/1_상수.py
+++ 3회차/이성훈/20220725/1_상수.py
@@ -2,3 +2,19 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+# 변수 A와 B에 문자 입력
+A , B = input().split()
+# A 와 B의 문자를 뒤집음(C와 D에 각각 넣음)
+C = A[::-1]
+D = B[::-1]
+# C 와 D의 값을 상수로 변환
+C = int(C)
+D = int(D)
+# C 와 D의 값을 서로 비교
+# C 가 D보다 크거나 같을 경우 C출력
+if C >= D:
+    print(C)
+else:
+    print(D)
+
