7f70a31b74edfcf18519fa832557c9b41d8b9fbb
kimdakyeom <kdk9912@naver.com>
2022-07-25 14:35:46 +0900

    0725 실습

--- 1회차/김다겸/20220725/2_두개뽑아서더하기.py
+++ 1회차/김다겸/20220725/2_두개뽑아서더하기.py
@@ -1,6 +1,20 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
     answer = []
+
+    # numbers의 길이만큼 순회
+    for i in range(len(numbers)):
+        # i의 다음 수부터 numbers의 길이만큼 순회
+        for j in range(i+1, len(numbers)):
+            # answer 리스트에 i 인덱스의 값과 j 인덱스의 값을 더해서 추가
+            answer.append(numbers[i] + numbers[j])
+
+    # answer 리스트에서 중복을 제거하기 위해 set함수 사용
+    answer = set(answer)
+    # 답을 리스트로 반환하기 위해 list함수 사용
+    answer = list(answer)
+    # 답을 오름차순으로 나타내기 위해 sort() 함수 사용
+    answer.sort()
     return answer
 
 
--- 1회차/김다겸/20220725/3_OX퀴즈.py
+++ 1회차/김다겸/20220725/3_OX퀴즈.py
@@ -2,3 +2,28 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+n = int(input())
+
+for t in range(1, n+1):
+    # ox를 문자열 리스트 형태로 저장
+    ox = list(input())
+    # 정답 변수 0으로 초기화
+    sum = 0
+    # 'O'가 연속으로 나올때까지 더할 변수 0으로 초기화
+    tmp = 0
+
+    # ox 리스트를 순회
+    for i in ox:
+        # ox가 'O'라면
+        if i == "O":
+            # tmp에 1씩 증가
+            tmp += 1
+            # 정답 변수에 tmp 더하기
+            sum += tmp
+        # xo가 'X'라면
+        elif i == "X":
+            # tmp를 0으로 초기화
+            tmp = 0
+
+    print(sum)
