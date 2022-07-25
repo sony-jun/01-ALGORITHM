220f5e727fe1d5e2bdc7c0b8cda0125eb96d077a
lsb0709 <dltkdqor0709@gmail.com>
2022-07-25 17:40:05 +0900

    0725

--- 2회차/이상백/20220725/0_숫자의개수.py
+++ 2회차/이상백/20220725/0_숫자의개수.py
@@ -1,4 +1,13 @@
 # https://www.acmicpc.net/problem/2577
-import sys
+# import sys
 
-sys.stdin = open("0_숫자의개수.txt")
+# sys.stdin = open("0_숫자의개수.txt", "r")
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+result = list(str(A * B * C))
+
+for mul in range(0, 10):
+    print(result.count(str(mul)))
--- 2회차/이상백/20220725/2_두개뽑아서더하기.py
+++ 2회차/이상백/20220725/2_두개뽑아서더하기.py
@@ -1,8 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
     answer = []
-    return answer
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            print(j)
+            answer.append(numbers[i] + numbers[j])
+
+    return sorted(list(set(answer)))
 
 
 print(solution([2, 1, 3, 4, 1]))
 print(solution([5, 0, 2, 7]))
+
