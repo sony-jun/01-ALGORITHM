78ccb84e6022f4888834b3eebcdd9190ec663687
dortkthf <dortkthf@gmail.com>
2022-07-25 16:14:11 +0900

    Algorithm 풀이

--- 2회차/이준엽/20220725/0_숫자의개수.py
+++ 2회차/이준엽/20220725/0_숫자의개수.py
@@ -2,3 +2,31 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+# 각각 한줄로 변수 A, B, C 를 받아야 하므로 각각 input을 한줄로 받기
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+# 각각의 숫자에 해당되는 값의 개수를 기록하기위해 빈 리스트를 생성
+
+number_cnt = list()
+
+# 결과값을 문자열로 바꿔놓기
+
+result = str(A*B*C)
+
+# 빈리스트에 인덱스 0부터 9까지 0을 할당해놓기
+
+for i in range(10):
+    number_cnt.append(0)
+
+# str의 result에서 i의 개수를 세기위해서 빈리스트의 해당 인덱스에 +1을 실행
+
+for i in result:
+    number_cnt[int(i)]+=1
+
+# 완성된 리스트를 하나씩 출력
+for i in number_cnt:
+    print(i)
--- 2회차/이준엽/20220725/1_상수.py
+++ 2회차/이준엽/20220725/1_상수.py
@@ -2,3 +2,14 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+A, B = input().split()
+
+A_reverse = int(A[::-1])
+B_reverse = int(B[::-1])
+
+if A_reverse > B_reverse :
+    print(A_reverse)
+else :
+    print(B_reverse)
+
