a0e42d420271396bd342a93256bbff988131e8b6
hady1024 <hady1024@gmail.com>
2022-07-25 23:43:23 +0900

    알고리즘 실습

--- 2회차/하두연/20220725/2_두개뽑아서더하기.py
+++ 2회차/하두연/20220725/2_두개뽑아서더하기.py
@@ -1,8 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
     answer = []
+
+    for e in range(0,len(numbers)):
+        for i in range(e+1,len(numbers)):
+            answer.append(numbers[e] + numbers[i])
+        answer = list(set(answer))
+        answer.sort()
+
     return answer
 
 
 print(solution([2, 1, 3, 4, 1]))
 print(solution([5, 0, 2, 7]))
+
+# 이 문제는 구글링
+ 
