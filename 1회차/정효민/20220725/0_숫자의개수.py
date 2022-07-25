30d82189181221e50d77bf5ee466671b5b43ce31
lol567236 <lol567236@gmail.com>
2022-07-26 00:41:03 +0900

    7월 25일 알고리즘

--- 1회차/정효민/20220725/0_숫자의개수.py
+++ 1회차/정효민/20220725/0_숫자의개수.py
@@ -1,4 +1,9 @@
 # https://www.acmicpc.net/problem/2577
-import sys
-
-sys.stdin = open("0_숫자의개수.txt")
+a , b , c = map(int, input().split()) # a b c의 인풋을 받아오는 코드
+y = a * b * c # a b c를 곱한것은 y
+x = [0] * 10 #리스트 자료형 [0 을 10개 생성]
+while y != 0: #a b c를 곱한 값이 0이 아니면 트루(0될떄까지 식을 계속)
+    x[y % 10] += 1 
+    y //= 10
+#for i in x:
+#    print(i) 
