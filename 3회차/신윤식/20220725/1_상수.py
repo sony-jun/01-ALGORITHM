8d431602e1181f08e6b3b380cd20a15878b6cd31
Yoonsik-Shin <sys25800994@gmail.com>
2022-07-25 17:57:46 +0900

    1기_3회차_신윤식

--- 3회차/신윤식/20220725/1_상수.py
+++ 3회차/신윤식/20220725/1_상수.py
@@ -1,4 +1,15 @@
 # https://www.acmicpc.net/problem/2908
 import sys
 
-sys.stdin = open("1_상수.txt")
+sys.stdin = open("./3회차/신윤식/20220725/1_상수.txt")
+
+A, B = input().split()  # 문자열로 받음
+A_R = int(A[::-1])      # 슬라이싱을 이용하여 문자열 순서를 뒤집고 숫자로 전환
+B_R = int(B[::-1])
+
+if A_R > B_R:           # 더 큰 값 대소비교
+    print(A_R)
+else:
+    print(B_R)
+
+# print(A_R) if A_R > B_R else print(B_R)
--- 3회차/신윤식/20220725/3_OX퀴즈.py
+++ 3회차/신윤식/20220725/3_OX퀴즈.py
@@ -1,4 +1,41 @@
 # https://www.acmicpc.net/problem/8958
 import sys
 
-sys.stdin = open("3_OX퀴즈.txt")
+sys.stdin = open("./3회차/신윤식/20220725/3_OX퀴즈.txt")
+
+
+# 1. 리스트 활용
+T = int(input())
+
+for i in range(T):
+
+    wrong_correts = input()
+    count = 0
+    lst = []
+
+    for wrong_corret in wrong_correts:
+        if wrong_corret == 'O':
+            count+=1
+            lst.append(count)
+        elif wrong_corret == 'X':
+            count = 0
+
+    print(sum(lst))
+
+    
+# 2. 변수 활용
+n = int(input())
+num=1
+total=0
+
+for i in range(n):
+    x = input()
+    for j in x:
+        if j == 'O':
+            total += num
+            num += 1
+        elif j == 'X':
+            num = 1
+    print(total)
+    num = 1
+    total=0
