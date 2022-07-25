2d2ae626e1d6f378c472803550e906156b4fc39e
kyominkim1074 <kyominkim1074@gmail.com>
2022-07-25 15:14:14 +0900

    0725 실습 풀이

--- 1회차/김교민/20220725/0_숫자의개수.py
+++ 1회차/김교민/20220725/0_숫자의개수.py
@@ -2,3 +2,12 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+re = str(a*b*c) #a b c를 곱한값을 문자열로 바꾼다.
+
+for i in range(10):
+    print(re.count(str(i)))
--- 1회차/김교민/20220725/3_OX퀴즈.py
+++ 1회차/김교민/20220725/3_OX퀴즈.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+t = int(input())
+for test_case in range(1, t+1):
+    l = list(input()) #입력된 값을 리스트로 만들기
+    score = 0 #점수
+    reset = 0 #O면 1씩 플러스하면서 점수에 추가하고 x면 0으로 초기화 시키는 값
+    for r in l:
+        if r == 'O': #O일 경우에
+            reset += 1 #계속 1씩 추가하고
+            score += reset #score에 더하기
+        else:
+            reset = 0 #X일 경우에 0으로
+    print(score)
