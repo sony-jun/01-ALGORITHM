078b3baaa466e3cf95221a89a17e8a28c01220b3
HYUNSIK-JI <https://github.com/HYUNSIK-JI>
2022-07-25 14:01:04 +0900

    2022_07_25 과제

--- 2회차/지현식/20220725/0_숫자의개수.py
+++ 2회차/지현식/20220725/0_숫자의개수.py
@@ -1,4 +1,17 @@
-# https://www.acmicpc.net/problem/2577
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+input = sys.stdin.readline
+
+mul = 1
+answer = [0] * 10
+
+for _ in range(3):
+    num = int(input())
+    mul *= num
+
+for i in str(mul):
+    answer[int(i)] += 1
+
+for i in answer:
+    print(i)
--- 2회차/지현식/20220725/1_상수.py
+++ 2회차/지현식/20220725/1_상수.py
@@ -1,4 +1,9 @@
-# https://www.acmicpc.net/problem/2908
 import sys
 
 sys.stdin = open("1_상수.txt")
+input = sys.stdin.readline
+
+A, B = map(str, input().rstrip().split())
+A, B = int(A[::-1]), int(B[::-1])
+
+print(A) if A > B else print(B)
