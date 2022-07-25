154d1090bfb1a10d924b1c104c77778e56b0b181
forwardyoung <chahwayoung214@gmail.com>
2022-07-25 23:22:13 +0900

    0725_algorithm

--- 1회차/차화영/20220725/1_상수.py
+++ 1회차/차화영/20220725/1_상수.py
@@ -2,3 +2,11 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+a, b = input().split()
+# 문자열을 슬라이싱을 할 것이기 때문에 map(int, )를 사용하지 않았음
+back_ab = [a[::-1], b[::-1]]
+# a와 b 각각을 거꾸로 하여 back_ab라는 리스트에 담는다.
+n = sorted(back_ab, reverse= True)
+# back_ab를 sorted 함수를 이용하여 내림차순으로 정렬
+print(n[0])
+# 리스트에서 인덱스가 0인 첫번째 숫자가 더 크다.
--- 1회차/차화영/20220725/3_OX퀴즈.py
+++ 1회차/차화영/20220725/3_OX퀴즈.py
@@ -2,3 +2,22 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+T = int(input())
+
+for test_case in range(1, T+1):
+    OX = input()
+    score = 0 
+    point = 0
+    for i in range(len(OX)):
+        if OX[i] == 'O':
+            point += 1
+            # O가 나올 때마다 point 1씩 증가
+            score = score + point
+            # score에 point 추가
+        else:
+            point = 0
+            # X가 나오면 point가 0이라 다시 O가 나오면 1부터 시작해야 됨
+    print(score)
+
+    
+    
