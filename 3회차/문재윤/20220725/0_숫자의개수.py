c31747338a15ebaa442b176124ab75270d9e8ba8
tenedict <tenedict@gmail.com>
2022-07-25 16:33:03 +0900

    0725프로젝트 제출

--- 3회차/문재윤/20220725/0_숫자의개수.py
+++ 3회차/문재윤/20220725/0_숫자의개수.py
@@ -2,3 +2,52 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+a = int(input())
+b = int(input())
+c = int(input())
+n = a*b*c
+n = str(n)
+a0 = 0
+a1 = 0
+a2 = 0
+a3 = 0
+a4 = 0
+a5 = 0
+a6 = 0
+a7 = 0
+a8 = 0
+a9 = 0
+
+for i in n:
+    if int(i) == 0:
+        a0 += 1
+    elif int(i) == 1: 
+        a1 += 1
+    elif int(i) == 2: 
+        a2 += 1
+    elif int(i) == 3: 
+        a3 += 1
+    elif int(i) == 4: 
+        a4 += 1
+    elif int(i) == 5: 
+        a5 += 1
+    elif int(i) == 6: 
+        a6 += 1
+    elif int(i) == 7: 
+        a7 += 1
+    elif int(i) == 8: 
+        a8 += 1
+    else:
+        a9 += 1
+
+print(a0)
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+print(a5)
+print(a6)
+print(a7)
+print(a8)
+print(a9)
