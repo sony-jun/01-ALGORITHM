38fc11b95e6c4bd8e1fe5efa6db344608345e5f6
min <smh92333@gmail.com>
2022-07-25 16:25:20 +0900

    0725

--- 2회차/장성민/20220725/0_숫자의개수.py
+++ 2회차/장성민/20220725/0_숫자의개수.py
@@ -2,3 +2,13 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+abc = []
+
+for three in range(3):
+    abc.append(int(input()))
+    
+abc = list(str(abc[0] * abc[1] * abc[2]))
+
+for i in range(10):
+    print(abc.count(str(i)))
