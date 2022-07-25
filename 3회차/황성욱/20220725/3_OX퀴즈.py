88ce0c4438f2572445d7faeba0f81bd1840d021a
wook <ifol1129@gmail.com>
2022-07-25 14:01:54 +0900

    0725 Prac

--- 3회차/황성욱/20220725/3_OX퀴즈.py
+++ 3회차/황성욱/20220725/3_OX퀴즈.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+n = int(input())
+for _ in range(n):
+    s = input()
+    sum_val = 0
+    cnt = 0
+    for i in s:
+        if i == 'O':
+            cnt += 1
+            sum_val += cnt
+        else:
+            cnt = 0
+    print(sum_val)
+
