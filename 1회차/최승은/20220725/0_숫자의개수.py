81067845874cb45a98ac79717ab7ef98b824c8b3
cse1940 <se1940@naver.com>
2022-07-25 14:59:20 +0900

    1차

--- 1회차/최승은/20220725/0_숫자의개수.py
+++ 1회차/최승은/20220725/0_숫자의개수.py
@@ -2,3 +2,20 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+number = str(a*b*c)
+
+print(number.count('0'))
+print(number.count('1'))
+print(number.count('2'))
+print(number.count('3'))
+print(number.count('4'))
+print(number.count('5'))
+print(number.count('6'))
+print(number.count('7'))
+print(number.count('8'))
+print(number.count('9'))
--- 1회차/최승은/20220725/1_상수.py
+++ 1회차/최승은/20220725/1_상수.py
@@ -2,3 +2,12 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+a, b = input().split()
+
+c = a[::-1]
+d = b[::-1]
+
+if c > d:
+    print(c)
+else:
+    print(d)
