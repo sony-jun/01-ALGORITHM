e495915c724d5052f24ef47c36b8a12bbdec1a00
knuksg <ksgyo0121@gmail.com>
2022-07-25 15:29:09 +0900

    0-3 완료

--- 3회차/김선교/20220725/3_OX퀴즈.py
+++ 3회차/김선교/20220725/3_OX퀴즈.py
@@ -2,3 +2,23 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+a = int(input())
+m = []
+while len(m) < a:
+    m.append(list(input().strip()))
+cnt = 0
+score = 0
+scores = []
+for ii in range(len(m)):
+    for i in m[ii]:
+        if i == 'O':
+            score += (1+cnt)
+            cnt += 1
+        else:
+            cnt = 0
+    scores.append(score)
+    score = 0
+    cnt = 0
+for i in range(a):
+    print(scores[i])
