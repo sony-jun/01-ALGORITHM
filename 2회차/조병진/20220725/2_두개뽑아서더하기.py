fb065b989d07e6561425dd3ed5d6820c9f1cc96d
byeongjin <qudwls7468@gmail.com>
2022-07-25 14:24:05 +0900

    2번 문제풀이 완료

--- 2회차/조병진/20220725/2_두개뽑아서더하기.py
+++ 2회차/조병진/20220725/2_두개뽑아서더하기.py
@@ -1,6 +1,22 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
+
+# 맨 아래 정의한 수가 numbers로 입력
 def solution(numbers):
+    # 결과를 담을 리스트 박스
     answer = []
+
+    # 길이만큼 반복
+    for i in range(len(numbers)):
+        # 길이만큼 반복하는데 i 다음의 수부터 시작
+        for j in range(i+1, len(numbers)):
+            # 두 수를 더한 값이 answer 안에 없다면
+            if numbers[i] + numbers[j] not in answer:
+                # 더한 값을 추가
+                answer.append(numbers[i] + numbers[j])
+
+    # 오름차순으로 정렬
+    answer.sort()
+
     return answer
 
 
