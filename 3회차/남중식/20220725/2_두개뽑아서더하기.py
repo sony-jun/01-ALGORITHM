9692dbae5152cbb1e2cbf32fa5b1329551c8092a
ForLunch01 <crazylunch2019@gmail.com>
2022-07-25 14:18:35 +0900

    알고리즘 풀이 0725

--- 3회차/남중식/20220725/2_두개뽑아서더하기.py
+++ 3회차/남중식/20220725/2_두개뽑아서더하기.py
@@ -1,8 +1,40 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
     answer = []
+    
+    # 첫번째 for문, 두 수 중 첫번째 수를 뽑는 for문
+    for i in range(len(numbers)):
+        # 두번째 수를 뽑는 for문, 첫번째 수 바로 다음 수부터 마지막까지의 수
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i]+numbers[j])
+            
+    answer = list(set(answer))
+    answer.sort()
+    
+    return answer
+
+from itertools import combinations
+
+def solution2(numbers):
+
+    answer = []
+    
+    # 조합, numbers 배열 중 2개를 뽑음
+    cb = combinations(numbers, 2)
+    
+    # 그 조합 수들의 합을 answer에 넣음
+    for c in cb:
+        answer.append(sum(c))
+    
+    answer = list(set(answer))
+    answer.sort()
+    
     return answer
 
 
-print(solution([2, 1, 3, 4, 1]))
-print(solution([5, 0, 2, 7]))
+print(solution2([2, 1, 3, 4, 1]))
+print(solution2([5, 0, 2, 7]))
+
+# 정수 배열 numbers
+# 서로 다른 인덱스에 있는 2개의 수를 뽑아 더해 만들 수 있는 수
+# 이 수들의 오름차순 정렬 배열
--- 3회차/남중식/20220725/3_OX퀴즈.py
+++ 3회차/남중식/20220725/3_OX퀴즈.py
@@ -2,3 +2,24 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+T = int(input())
+
+for i in range(T):
+    case = input()
+    
+    # count : O가 연속될 때 점수가 올라가는 것을 체크하기 위함
+    count = 0
+    
+    # result : 총점을 계산하기 위함
+    result = 0
+    for c in case:
+        # 현재 문자가 X면 o에 대한 count를 0으로 만듬
+        if c == 'X':
+            count = 0   
+        # 현재 문자가 o면 o에 대한 count를 올리고 총점에 더함
+        elif c == 'O':
+            count = count + 1
+            result = result + count
+    print(result)
+    
