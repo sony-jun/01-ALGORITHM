1c5422be36176539e41bacbe7621ba731e413658
SHIN.sped <105416148+SHIN-sped@users.noreply.github.com>
2022-07-25 16:58:29 +0900

    project

--- 1회차/신현강/20220725/2_두개뽑아서더하기.py
+++ 1회차/신현강/20220725/2_두개뽑아서더하기.py
@@ -1,8 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
+    l=len(numbers)
     answer = []
+    index=0
+    for i in range(l-1):
+        for j in range(i+1, l):
+            answer.append(numbers[i] + numbers[j])
+            
+    answer=list(set(answer))
+    answer.sort()
     return answer
-
-
 print(solution([2, 1, 3, 4, 1]))
 print(solution([5, 0, 2, 7]))
