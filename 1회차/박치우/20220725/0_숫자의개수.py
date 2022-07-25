8c91b9e86b355bf4628637b8c7692ce08a60fdd9
chiwoogood <chiwooooo@naver.com>
2022-07-25 15:01:35 +0900

    Algorithm day 1

--- 1회차/박치우/20220725/0_숫자의개수.py
+++ 1회차/박치우/20220725/0_숫자의개수.py
@@ -1,4 +1,20 @@
-# https://www.acmicpc.net/problem/2577
 import sys
 
+
 sys.stdin = open("0_숫자의개수.txt")
+lst = []
+for i in range(3):
+    k = int(input())
+    lst.append(k)
+mul = lst[0] * lst[1] * lst[2]
+mul = str(mul)
+dic = {}
+for i in mul:
+    if i in dic :
+        dic[i] += 1
+    else :
+        dic[i] = 1
+print(dic)
+
+
+
--- 1회차/박치우/20220725/3_OX퀴즈.py
+++ 1회차/박치우/20220725/3_OX퀴즈.py
@@ -2,3 +2,20 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+
+n = int(input())
+for i in range(n):
+    quiz = input()
+    score = 0
+    s_score = 0
+    for j in quiz:
+        if j == 'O':
+            s_score +=1
+            score += s_score
+        else :
+            s_score = 0
+    print(score)
+    
+
+            
