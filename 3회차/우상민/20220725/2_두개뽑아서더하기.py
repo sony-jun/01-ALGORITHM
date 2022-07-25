fc09ae4f8a817d2cb467c2656483c4ec0d78e102
wsm0409 <wsm0409@naver.com>
2022-07-25 22:54:44 +0900

    checkup0725

--- 3회차/우상민/20220725/2_두개뽑아서더하기.py
+++ 3회차/우상민/20220725/2_두개뽑아서더하기.py
@@ -1,8 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
     answer = []
-    return answer
+    for idx in range(len(numbers)):
+        for second in range(idx+1, len(numbers)):
+            answer.append((numbers[idx] + numbers[second]))
+    return sorted(list(set(answer)))
+    
+
+
 
 
 print(solution([2, 1, 3, 4, 1]))
 print(solution([5, 0, 2, 7]))
+
--- 3회차/우상민/20220725/3_OX퀴즈.py
+++ 3회차/우상민/20220725/3_OX퀴즈.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+T = int(input())               
+for R in range(1, T+1):
+    A = input()               
+    count = 0
+    total = 0
+    for i in A:
+        if i == ('O'):         
+            count += 1        
+            total += count   
+        else:
+            count = 0         
+    print(total)
