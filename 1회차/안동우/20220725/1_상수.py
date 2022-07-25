9f61ff359daed2669445685c74ea172662d04586
an dong woo <akskwha@naver.com>
2022-07-25 16:48:12 +0900

    제출

--- 1회차/안동우/20220725/1_상수.py
+++ 1회차/안동우/20220725/1_상수.py
@@ -2,3 +2,18 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+#입력 값 거꾸로 비교하고
+#출력
+
+A, B =map(int, input().split())
+
+A=A
+compare1=(A//100) + (((A% 100)// 10)*10) +((A% 10)*100)
+
+B=B
+compare2=(B//100) + (((B% 100)// 10)*10) +((B% 10)*100)
+#거꾸로
+if compare1>compare2:
+    print(compare1)
+else:
+    print(compare2)
--- 1회차/안동우/20220725/3_OX퀴즈.py
+++ 1회차/안동우/20220725/3_OX퀴즈.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+#연속으로 정답이 점수가  1씩증가
+
+T=int(input())
+a= 'O'#정답
+b= 'X'#오답
+score=0
+
+d=0
+z=0
+x=0
+N=input()# 정답오답 입력
+list(N)
+print(N, sep='')
