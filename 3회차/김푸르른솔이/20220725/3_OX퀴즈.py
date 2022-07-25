cff35a4a60afb41bb4abb0319845dc6f3adcb668
kfrrsi0301 <kfrrsi0301@naver.com>
2022-07-25 17:54:32 +0900

    0725 수업실습

--- 3회차/김푸르른솔이/20220725/3_OX퀴즈.py
+++ 3회차/김푸르른솔이/20220725/3_OX퀴즈.py
@@ -2,3 +2,31 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+a = int(input())
+for i in range(a):
+    b = input()
+    s = list(b)
+    score = 0
+    sum_score = 0
+    for ox in s:
+        if  ox == 'O':
+            score += 1
+            sum_score += score
+        else:
+            score = 0
+    print(sum_score)
+
+
+# a = int(input())
+# for i in range(a):
+#     b = input()
+#     score = 0
+#     sumscore = 0
+#     for j in b:
+#         if j == 'O':
+#             score += 1
+#         else:
+#             score = 0
+#         sumscore += score
+#     print(sumscore)
