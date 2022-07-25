a90586283f0c09c769d4d0b234e04800d451e7cf
yangu1455 <kunja266@hanmail.net>
2022-07-25 14:06:07 +0900

    8958번 OX 퀴즈

--- 2회차/황지선/20220725/3_OX퀴즈.py
+++ 2회차/황지선/20220725/3_OX퀴즈.py
@@ -1,4 +1,18 @@
 # https://www.acmicpc.net/problem/8958
-import sys
+# 8958번 OX 퀴즈
 
-sys.stdin = open("3_OX퀴즈.txt")
+n= int(input())
+
+for i in range(n):
+    a= input()
+    b= list(a)
+    sum = 0
+    x = 1
+
+    for i in b:
+        if i == 'O':
+            sum += x
+            x += 1
+        else:
+            x = 1
+    print(sum)
