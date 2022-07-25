761e5d8ec0e45709a80642dd730891a6aef4bfca
wxxk <dwde22@gmail.com>
2022-07-25 17:56:31 +0900

    문제풀이

--- 1회차/이상욱/20220725/0_숫자의개수.py
+++ 1회차/이상욱/20220725/0_숫자의개수.py
@@ -2,3 +2,12 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+A = int(input())
+B = int(input())
+C = int(input())
+number = A * B * C
+list= list(str(number))
+
+for i in range(10):
+    print(list.count(str(i)))
--- 1회차/이상욱/20220725/1_상수.py
+++ 1회차/이상욱/20220725/1_상수.py
@@ -2,3 +2,10 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+A, B = map(str, input().split())
+
+if A[::-1] > B[::-1]:
+    print(A[::-1])
+elif A[::-1] < B[::-1]:
+    print(B[::-1])
