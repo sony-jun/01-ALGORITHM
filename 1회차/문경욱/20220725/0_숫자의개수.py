4bbdff17bc3f4c2e1765dba28918ec15b37f7a1a
9776mk <9776mk@gmail.com>
2022-07-25 14:20:40 +0900

    0725 실습

--- 1회차/문경욱/20220725/0_숫자의개수.py
+++ 1회차/문경욱/20220725/0_숫자의개수.py
@@ -1,4 +1,23 @@
 # https://www.acmicpc.net/problem/2577
-import sys
 
-sys.stdin = open("0_숫자의개수.txt")
+# A, B, C 세개의 입력을 받음
+A = int(input())
+B = int(input())
+C = int(input())
+
+# 세 수를 곱한뒤, 문자열로 사용하기 위해 str로 형변환
+result = A * B * C
+result = str(result)
+
+# 0~9까지 넣을 수 있는 딕셔너리를 만듦.
+num_dict = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+
+# 만약 num가 num_dict에 있는 key와 일치하면
+for num in result:
+    if num in num_dict.keys():
+        # num_dict의 value를 +1
+        num_dict[num] += 1
+
+# 딕셔너리의 value들을 출력
+for num in num_dict:
+    print(num_dict[num])
--- 1회차/문경욱/20220725/3_OX퀴즈.py
+++ 1회차/문경욱/20220725/3_OX퀴즈.py
@@ -1,4 +1,41 @@
 # https://www.acmicpc.net/problem/8958
-import sys
+# 테스트 케이스를 입력 받음
+T = int(input())
 
-sys.stdin = open("3_OX퀴즈.txt")
+# 테스트 케이스만큼 반복
+for test_case in range(T):
+    # O와 X 문자열을 ans로 입력받음
+    ans = input()
+    # o를 집어넣을 o_list를 선언
+    o_list = []
+    # 전체 합과 임시 합을 선언
+    sum_num = 0
+    sum_tmp = 0
+
+    # 입력 개수만큼 반복문 진행
+    for i in range(len(ans)):
+
+        # 문자가 'O'일 때
+        if ans[i] == 'O':
+            # o_list에 O를 추가
+            o_list.append('O')
+
+            # i가 마지막일 때, O인 경우 더하기가 진행이 되지 않으므로 i가 마지막인 경우 추가
+            if i == len(ans)-1: 
+                for i in range(1, len(o_list)+1):
+                    sum_tmp += i
+                sum_num += sum_tmp
+        # 문자가 X일 때
+        else:
+            # o_list가 비어있을 때는 그냥 진행
+            if len(o_list) == 0:
+                continue
+            # o_list가 비어있지 않을 때는
+            else:
+                # 1,2,3, 씩 O 갯수만큼 sum_tmp에 담아 전체 sum에 저장한 후 다시 sum_tmp를 0으로 변경, o_list 초기화
+                for i in range(1, len(o_list)+1):
+                    sum_tmp += i
+                sum_num += sum_tmp
+                sum_tmp = 0
+                o_list = []
+    print(sum_num)
