acd480fcc24fa197e475ba1e159d5a67bfa256e7
ererink <yelkim0210@gmail.com>
2022-07-25 16:34:40 +0900

    2회차 김예린 제출

--- 2회차/김예린/20220725/0_숫자의개수.py
+++ 2회차/김예린/20220725/0_숫자의개수.py
@@ -1,4 +1,21 @@
-# https://www.acmicpc.net/problem/2577
 import sys
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = a * b * c
+result = str(result)
+
+print(result.count('0'))
+print(result.count('1'))
+print(result.count('2'))
+print(result.count('3'))
+print(result.count('4'))
+print(result.count('5'))
+print(result.count('6'))
+print(result.count('7'))
+print(result.count('8'))
+print(result.count('9'))
 
 sys.stdin = open("0_숫자의개수.txt")
--- 2회차/김예린/20220725/3_OX퀴즈.py
+++ 2회차/김예린/20220725/3_OX퀴즈.py
@@ -1,4 +1,21 @@
 # https://www.acmicpc.net/problem/8958
-import sys
+#import sys
 
-sys.stdin = open("3_OX퀴즈.txt")
+t = int(input())
+for test_case in range(1, t + 1): # 테스트 케이스 횟수
+    
+    ox_list = list(input()) # 입력한 OX를 리스트로 만들어준다. 
+    cnt = 0
+    result = 0
+    for i in ox_list:       # i에 O,X가 차례대로 들어온다. 
+        if i == 'O':        # 만약 i에 O가 들어왔다면 
+            cnt += 1        # cnt에 1을 추가한다
+            result += cnt      # cnt의 값을 fin에 넣어준다. 
+                            # 연속적인 O가 들어오는 경우 cnt는 1씩 늘어나기 때문에
+                            # 연속적인 숫자 (1+2..) 합계가 가능하다. 
+        else:
+            cnt = 0         # X가 i에 들어오는 경우, cnt를 0으로 비워준다. 
+    
+    print(result)
+#sys.stdin = open("3_OX퀴즈.txt")
+
