40d2d1cdb4d83b2204def1e04c1610834a6a6e88
buzzzlight <pomodoro5292@gmail.com>
2022-07-25 16:11:32 +0900

    1기 2회차 김수빈 0725

--- 2회차/김수빈/20220725/1_상수.py
+++ 2회차/김수빈/20220725/1_상수.py
@@ -2,3 +2,13 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+A, B = map(int, input().split())
+
+A_reverse = str(A)[::-1]
+B_reverse = str(B)[::-1]
+
+if int(A_reverse) > int(B_reverse):
+    print(A_reverse)
+else:
+    print(B_reverse)
