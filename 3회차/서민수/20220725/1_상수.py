fe221feb009d7ba1399502b2dc7abaedffbe0b15
TocDX <sk372021000@gmail.com>
2022-07-25 17:48:48 +0900

    프로그래머스 제외  문제 풀이

--- 3회차/서민수/20220725/1_상수.py
+++ 3회차/서민수/20220725/1_상수.py
@@ -1,4 +1,24 @@
-# https://www.acmicpc.net/problem/2908
+
+# https://www.acmicpc.net/problem/2577
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+# 두 수를 비교하기 위해 input 함수를 받는다
+# 상수는 수를 거꾸로 읽는다 734는 437로 읽는다면 생각나는건 리스트 뒤집기 (::-1)
+# 두 수를 비교해보자
+
+# a,b를 받을 함수
+a, b = input().split()
+
+# 각 a와 b를 리스트로 뒤집어준 후 인트 형변환
+a = a[::-1]
+b = b[::-1]
+
+# 만약 a가 b보다 크다면
+if a > b:
+    # a 출력
+    print(a)
+else:
+    # 그렇지 않다면 b 출력
+    print(b)
--- 3회차/서민수/20220725/2_두개뽑아서더하기.py
+++ 3회차/서민수/20220725/2_두개뽑아서더하기.py
@@ -1,6 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
+
+
+# 리스트 안에 있는 각 수들을 더하여 나올 수 있는 모든 수를 도출해라
+# 1+1 = 2, 2+1 = 3, 3+1 = 4 등
+# 오름차순 배열 sort()
+
 def solution(numbers):
     answer = []
+    for i in range(len(numbers)):
+        if 
+
     return answer
 
 
