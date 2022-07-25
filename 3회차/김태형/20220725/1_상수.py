5dd2002c827fffe1cedfa88f6798ae232ec95194
riumr <atrium_grate.0j@icloud.com>
2022-07-25 17:42:07 +0900

    220725 실습

--- 3회차/김태형/20220725/1_상수.py
+++ 3회차/김태형/20220725/1_상수.py
@@ -1,8 +1,4 @@
 # https://www.acmicpc.net/problem/2908
 
-A,B=input().split()
-
-if int(A[::-1])>int(B[::-1]):
-    print(int(A[::-1]))
-elif int(A[::-1])<int(B[::-1]):
-    print(int(B[::-1]))
+A, B = input().split()
+print(max(int(A[::-1],int(B[::-1]))))
