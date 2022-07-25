f96da4668dea33ec9c12754f2c8aa09fef0a3466
mechauk418 <mechauk418@gmail.com>
2022-07-25 14:32:44 +0900

    0725 이진욱 실습

--- 1회차/이진욱/20220725/0_숫자의개수.py
+++ 1회차/이진욱/20220725/0_숫자의개수.py
@@ -1,4 +1,14 @@
 # https://www.acmicpc.net/problem/2577
-import sys
+import sys, collections
 
 sys.stdin = open("0_숫자의개수.txt")
+
+A=int(input())
+B=int(input())
+C=int(input())
+
+num_list=list(str(A*B*C)) # A,B,C의 곱을 문자열로 변환하여 리스트로 변환한다.
+dict_num = collections.Counter(num_list) # Counter 클래스를 사용하여 리스트의 원소 개수를 딕셔너리로 반환
+
+for i in range(10):
+    print(dict_num[str(i)]) # 0 ~ 9까지 순회하면서 출력
--- 1회차/이진욱/20220725/2_두개뽑아서더하기.py
+++ 1회차/이진욱/20220725/2_두개뽑아서더하기.py
@@ -1,6 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
     answer = []
+    for i in range(len(numbers)-1):
+        for j in range(i+1,len(numbers)):
+            sum = numbers[i] + numbers[j] # 이중반복문을 통해 두 수의 합을 구한다.
+            answer.append(sum)
+
+    answer = list(set(answer)) # 중복을 제거한다.
+
     return answer
 
 
