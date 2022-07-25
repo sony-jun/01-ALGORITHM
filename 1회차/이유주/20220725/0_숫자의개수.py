b05bffcfac84f188e7850e3238809d1b86bfafb2
rkskakdlfem3 <rkskakdlfem3@gmail.com>
2022-07-25 18:01:15 +0900

    이유주 0725 문제풀이

--- 1회차/이유주/20220725/0_숫자의개수.py
+++ 1회차/이유주/20220725/0_숫자의개수.py
@@ -2,3 +2,15 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+
+#숫자가 몇번 쓰였는지 0부터 9까지 세기
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+multiply = list(str(A * B * C))
+
+for x in range(10):
+    print(multiply.count(str(x)))
--- 1회차/이유주/20220725/1_상수.py
+++ 1회차/이유주/20220725/1_상수.py
@@ -2,3 +2,11 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+A, B = input().split()
+rev_A = int(str(A)[::-1])
+rev_B = int(str(B)[::-1])
+if rev_A > rev_B:
+    print(rev_A)
+else: 
+    print(rev_B)
