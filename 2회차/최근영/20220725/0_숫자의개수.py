47900c65386ef6b94138bfb72bb81c157f007fee
choikeunyoung <ought119@gmail.com>
2022-07-25 18:02:06 +0900

    0,1,3 문제풀이

--- 2회차/최근영/20220725/0_숫자의개수.py
+++ 2회차/최근영/20220725/0_숫자의개수.py
@@ -2,3 +2,20 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+
+A = int(sys.stdin.readline())
+B = int(sys.stdin.readline())
+C = int(sys.stdin.readline())
+
+total = A*B*C
+total = str(total)
+dict = {}
+for j in range(0,10):
+    dict[str(j)] = 0
+for i in total:
+    if i in dict:
+        dict[i] += 1
+
+for k in dict:
+    print(dict[k])
