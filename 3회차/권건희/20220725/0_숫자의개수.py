84198df7c20091a71f4d30beaf96bfe54adf8855
Gkhy <syan1022@naver.com>
2022-07-25 17:46:22 +0900

    commit

--- 3회차/권건희/20220725/0_숫자의개수.py
+++ 3회차/권건희/20220725/0_숫자의개수.py
@@ -8,10 +8,12 @@ c=int(input())
 multi=a*b*c
 # 멀티를 str로 변환하고 {}생성, 인덱스로 접근, 해당 밸류에 +1하자
 multi=str(multi)
-numbers={'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
+numbers={}
+for i in range(10):
+    numbers[i]=0
 for n in multi:
     for i in range(10):
         if n==str(i):
-            numbers[str(i)]+=1
+            numbers[i]+=1
 for i in range(10):
-    print(numbers[str(i)])            
+    print(numbers[i])            
