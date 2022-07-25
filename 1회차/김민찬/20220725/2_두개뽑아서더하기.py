fa476cc63dd29d93d020bfd648921ba37130766c
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-25 16:43:42 +0900

    20220725_김민찬

--- 1회차/김민찬/20220725/2_두개뽑아서더하기.py
+++ 1회차/김민찬/20220725/2_두개뽑아서더하기.py
@@ -4,9 +4,9 @@ def solution(numbers):
 
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
-            answer.add(numbers[i] + numbers[j])
+            answer.add(numbers[i] + numbers[j]) # 두 숫자를 더한 값
     answer = list(answer) # answer를 리스트로 변환
-    answer.sort() # 정렬하기 위해 .sort()를 사용
+    answer.sort() # 정렬하기 위해 .sort()를 사용(오름차순)
 
     return answer
 
