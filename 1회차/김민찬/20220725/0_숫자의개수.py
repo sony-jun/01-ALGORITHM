d3d26f539b37722ea6aaa73ab38024a358d5fd0f
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-25 14:57:16 +0900

    20220725_김민찬

--- 1회차/김민찬/20220725/0_숫자의개수.py
+++ 1회차/김민찬/20220725/0_숫자의개수.py
@@ -2,3 +2,11 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+A = int(input()) # 150
+B = int(input()) # 266
+C = int(input()) # 427
+multiplication = list(str(A * B * C)) # [1, 7, 0, 3, 7, 3, 0, 0]
+
+for i in range(10): # i = 0 ~ 9
+    print(multiplication.count(str(i))) # i를 문자열(str)로 바꿔서 몇 번 쓰였는지 count
