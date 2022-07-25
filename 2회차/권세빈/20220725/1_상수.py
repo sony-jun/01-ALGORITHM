a5b57ac6853817c7a89efcbd0af33e6c918b7329
sebin kwon <sbkwon98@gmail.com>
2022-07-25 16:53:52 +0900

    2회차 권세빈 알고리즘 문제 풀이

--- 2회차/권세빈/20220725/1_상수.py
+++ 2회차/권세빈/20220725/1_상수.py
@@ -2,3 +2,9 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+a, b = input().split()
+# 문자열로 인풋 받기
+print(max(int(a[::-1]),int(b[::-1])))
+# 거꾸로 하고 정수형으로 변환, max로 최대값 구하기
+
--- 2회차/권세빈/20220725/2_두개뽑아서더하기.py
+++ 2회차/권세빈/20220725/2_두개뽑아서더하기.py
@@ -1,6 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
     answer = []
+    for i in range(len(numbers)):                   # 첫번째 숫자부터 마지막까지 순회하는 반복문
+        for j in range(i+1,len(numbers)):           # i 다음부터 순회하는 반복문
+            answer.append(numbers[i] + numbers[j])  # 리스트에 인덱싱으로 접근 후, 더한 값을 리스트에 추가함
+    answer = sorted(list(set(answer)))              # set으로 중복제거, 리스트 변환 후 정렬
     return answer
 
 
