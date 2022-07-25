1641ce206e5df6212beb921c772b1fedb52fbcfd
HYUNSIK-JI <https://github.com/HYUNSIK-JI>
2022-07-25 15:46:52 +0900

    추가 및 수정

--- 2회차/지현식/20220725/1_상수.py
+++ 2회차/지현식/20220725/1_상수.py
@@ -1,9 +1,14 @@
 import sys
 
-sys.stdin = open("1_상수.txt")
+# sys.stdin = open("1_상수.txt")
 input = sys.stdin.readline
 
 A, B = map(str, input().rstrip().split())
-A, B = int(A[::-1]), int(B[::-1])
 
-print(A) if A > B else print(B)
+for i in range(3):
+    if int(A[-i - 1]) < int(B[-i - 1]):
+        print(B[::-1])
+        break
+    elif int(A[-i - 1]) > int(B[-i - 1]):
+        print(A[::-1])
+        break
