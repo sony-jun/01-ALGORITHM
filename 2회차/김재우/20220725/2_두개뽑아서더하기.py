01bee6aa901d7787bb15aa017dad58c697100387
jaejae200 <kj31049@naver.com>
2022-07-26 01:14:39 +0900

    2회차 김재우 문제해결 및 후기

--- 2회차/김재우/20220725/2_두개뽑아서더하기.py
+++ 2회차/김재우/20220725/2_두개뽑아서더하기.py
@@ -1,6 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
-def solution(numbers):
-    answer = []
+def solution(numbers): 
+    answer = set()
+    for i in range(len(numbers)): #0 ~ numbers
+        for j in range(i+1, len(numbers)): # 1 ~ numbers
+            answer.add(numbers[i]+numbers[j]) # set에 number 값 두개 더한 정수를 입력
+    answer = list(answer)  # 리스트로 변환
+    answer.sort() # 정렬
     return answer
 
 
