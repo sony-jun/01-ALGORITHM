30e722152a2e2b34a9596898eb0951c006522288
1_1_LJS <jeongseob.korea@gmail.com>
2022-07-25 18:04:16 +0900

    finished_0725

--- 1회차/이정섭/20220725/1_상수.py
+++ 1회차/이정섭/20220725/1_상수.py
@@ -2,3 +2,14 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+    
+a, b = input().split()
+
+a = a[::-1]
+b = b[::-1]
+
+if a > b:
+    print(a)
+else:
+    print(b)
--- 1회차/이정섭/20220725/3_OX퀴즈.py
+++ 1회차/이정섭/20220725/3_OX퀴즈.py
@@ -2,3 +2,17 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+time = int(input())
+for t in range(1, time + 1):
+    prob = input()
+    total = 0
+    q = 1
+    for i in range(len(prob)):
+        if prob[i] == 'O':
+            if prob[i-1] == 'O' and (i != 0):
+                q += 1
+            else:
+                q = 1
+            total += q
+    print(total)
