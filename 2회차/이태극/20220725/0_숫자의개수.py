0202ead133508a7717db4b11cc2f4357e0032006
uRo3YA <ltg8693@gmail.com>
2022-07-25 14:35:11 +0900

    0725 실습

--- 2회차/이태극/20220725/0_숫자의개수.py
+++ 2회차/이태극/20220725/0_숫자의개수.py
@@ -2,3 +2,12 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+a = int(sys.stdin.readline())               
+b = int(sys.stdin.readline())                
+c = int(sys.stdin.readline())       
+
+result = list(str(a * b * c))   
+
+for i in range(10):            
+    print(result.count(str(i))) 
