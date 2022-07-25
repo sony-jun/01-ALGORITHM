3dca6831182488482620f924ae45e5307a36bfda
Gkhy <syan1022@naver.com>
2022-07-25 16:41:27 +0900

    complete

--- 3회차/권건희/20220725/0_숫자의개수.py
+++ 3회차/권건희/20220725/0_숫자의개수.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+a=int(input())
+b=int(input())
+c=int(input())
+multi=a*b*c
+# 멀티를 str로 변환하고 {}생성, 인덱스로 접근, 해당 밸류에 +1하자
+multi=str(multi)
+numbers={'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
+for n in multi:
+    for i in range(10):
+        if n==str(i):
+            numbers[str(i)]+=1
+for i in range(10):
+    print(numbers[str(i)])            
--- 3회차/권건희/20220725/1_상수.py
+++ 3회차/권건희/20220725/1_상수.py
@@ -2,3 +2,15 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+a,b=input().split()
+c=""
+d=""
+for i in a:
+    c=i+c
+for i in b:
+    d=i+d    
+if int(c)>int(d):
+    print(int(c))
+elif int(c)<int(d):
+    print(int(d))
