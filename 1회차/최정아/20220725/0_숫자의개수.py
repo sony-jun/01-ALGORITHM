71b8791c6f23e7780f058ae33775252414051e77
astroastrum <jungahchoi92@gmail.com>
2022-07-25 16:25:47 +0900

    0725 알고리즘

--- 1회차/최정아/20220725/0_숫자의개수.py
+++ 1회차/최정아/20220725/0_숫자의개수.py
@@ -2,3 +2,13 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+a = int(input()) #150 
+b = int(input()) #266
+c = int(input()) #427
+# 곱한 정수 값은 str으로 바꾼뒤에 리스트로 만든다
+result = list(str(a * b * c)) #[1, 7, 0, 3, 7, 3, 0, 0]
+
+for i in range(10): # i는 0~9까지 순회
+    #정수 타입을 문자열 타입으로 바꾸는 이유는 cnt 함수 사용하기 위함
+    print(result.count(str(i))) #i는 문자열 타입으로 바꿔서 count.
--- 1회차/최정아/20220725/1_상수.py
+++ 1회차/최정아/20220725/1_상수.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+# input은 기본적으로 문자열 받음
+a, b = input().split()
+# 리스트 슬라이싱해서 숫자 거꾸로 나열
+a = int(a[::-1])
+b = int(b[::-1])
+
+# 만약 a가 b보다 크면 a 출력
+# 아니면 b출력
+if a > b:
+    print(a)
+else:
+    print(b)
