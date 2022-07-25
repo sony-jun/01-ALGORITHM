71aa8bf5f80783af3777a61835b83f251645b029
midhyun <kfree912@gmail.com>
2022-07-25 18:00:49 +0900

    2회차_김현중_BOJ

--- 2회차/김현중/20220725/1_상수.py
+++ 2회차/김현중/20220725/1_상수.py
@@ -2,3 +2,12 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+C = input()
+lst = list(C)
+lst_rev = lst.reverse()
+rev_num = ''.join(lst)
+A, B = map(int, rev_num.split())
+if A > B :
+    print(A)
+else : print(B)
--- 2회차/김현중/20220725/2_두개뽑아서더하기.py
+++ 2회차/김현중/20220725/2_두개뽑아서더하기.py
@@ -1,7 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
-    answer = []
+    lst = []
+    for i in range(len(numbers)):
+        for j in range(i+1,len(numbers)):
+            lst.append(numbers[i]+numbers[j])
+    
+    answer = sorted(list(set(lst)))
     return answer
+
 
 
 print(solution([2, 1, 3, 4, 1]))
