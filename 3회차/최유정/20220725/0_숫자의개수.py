aff61d3a809443c7f5a36d73c154c4f2a8cad96c
yujeong6676 <stellachoi6676@gmail.com>
2022-07-25 16:36:33 +0900

    220725 최유정 commit

--- 3회차/최유정/20220725/0_숫자의개수.py
+++ 3회차/최유정/20220725/0_숫자의개수.py
@@ -2,3 +2,19 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+re = A * B * C
+
+count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for i in str(re):
+    count[int(i)] +=1
+
+for j in count:
+    print(j) 
+    
--- 3회차/최유정/20220725/3_OX퀴즈.py
+++ 3회차/최유정/20220725/3_OX퀴즈.py
@@ -2,3 +2,21 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+T=int(input())
+
+for t in range(T):
+    inputstr = input()
+
+   
+    result=0
+    o_cnt=0
+
+    for i in inputstr:
+    
+        if i == 'O':
+            result = result+1+o_cnt
+            o_cnt+=1
+        else:
+            o_cnt=0
+           
+    print(result)
