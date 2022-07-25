c8c05c1eff5d407ad2e312291c4c24c302a5fd81
GitHub etdong0820 <etd93@naver.com>
2022-07-26 02:56:46 +0900

    0725 풀이

--- 1회차/엄태동/20220725/1_상수.py
+++ 1회차/엄태동/20220725/1_상수.py
@@ -2,3 +2,18 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+a,b = map(int, input().split())
+cnt =0
+
+def changeValue(a):
+    n2=0
+    value = len(str(a))
+    for i in range(value):
+        n1 = a % 10
+        a = a//10
+        n2 += n1*(10**(value-1-i))
+    return n2
+if changeValue(a) >= changeValue(b):
+    print(changeValue(a))
+else:
+    print(changeValue(b))
--- 1회차/엄태동/20220725/2_두개뽑아서더하기.py
+++ 1회차/엄태동/20220725/2_두개뽑아서더하기.py
@@ -1,7 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
-    answer = []
-    return answer
+    new_list=[]
+    value=0
+    for i in range(len(numbers)-1):
+        value += 1 
+        for j in range(len(numbers)-(i+1)):
+            new_num = numbers[i] + numbers[j+value]  
+            new_list.append(new_num)
+        
+    new_list1 = sorted(set(new_list))
+    return new_list1
 
 
 print(solution([2, 1, 3, 4, 1]))
