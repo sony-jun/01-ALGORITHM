38e7809cd5060a71370c14f3dcdf5b08c6b79ce7
w00ye0l <dnduf158@gmail.com>
2022-07-25 17:24:38 +0900

    1기 2회차 이우열 20220725

--- 2회차/이우열/20220725/2_두개뽑아서더하기.py
+++ 2회차/이우열/20220725/2_두개뽑아서더하기.py
@@ -3,9 +3,9 @@ def solution(numbers):
     answer = []
 
     for i in range(len(numbers)):
-        for j in range(i, len(numbers)):
-            if i != j:                                  # 이중 for문을 사용하여 같은 인덱스가 아닌 수끼리 더해주고
-                answer.append(numbers[i] + numbers[j])  # 배열에 추가한다
+        # 이중 for문을 사용하여 같은 인덱스가 아닌 수끼리 더해주고
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])  # 배열에 추가한다
 
     # set로 중복을 없앤 뒤 리스트로 형 변환하여 정렬한다
     answer = sorted(list(set(answer)))
