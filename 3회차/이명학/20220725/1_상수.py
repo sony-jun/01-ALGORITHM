694464993de6e4c49978a73ca99988ff22245641
Myeonghak Lee <mhmh779@naver.com>
2022-07-25 16:47:03 +0900

    3회차 이명학 제출

--- 3회차/이명학/20220725/1_상수.py
+++ 3회차/이명학/20220725/1_상수.py
@@ -2,3 +2,31 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+a, b = input().split()
+
+a_list = []
+b_list = []
+for i in a:
+    a_list.append(i)
+for j in b:
+    b_list.append(j)
+
+temp = a_list[0]
+a_list[0] = a_list[2]
+a_list[2] = temp
+
+temp2 = b_list[0]
+b_list[0] = b_list[2]
+b_list[2] = temp2
+
+# a_list = ['4', '3', '7']
+# b_list = ['3', '9', '8']
+result = a_list[0]+a_list[1]+a_list[2]
+result2 = b_list[0]+b_list[1]+b_list[2]
+# 437
+# 398
+if result > result2:
+    print(result)
+else:
+    print(result2)
