e8677726598f1898c4368ab027a952aa271a45e1
ksy133900 <tkdgns13578@gmail.com>
2022-07-25 15:15:11 +0900

    0725 알고리즘 파이썬 실습

--- 1회차/김상훈/20220725/3_OX퀴즈.py
+++ 1회차/김상훈/20220725/3_OX퀴즈.py
@@ -2,3 +2,19 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+T = int(input())
+for a in range(T): # 테스트 케이스 갯수
+    
+    x = input() # 문자열 입력 받고
+    y = list(x) # 입력값을 리스트에 저장
+    sum = 0 # 점수값 = 0
+    Correct = 1 # 'O'을 만났을 때 sum을 1씩 증가시켜야 하므로 Correct 값은 1로 시작
+    
+    for i in y: # 리스트 내 문자열 순회
+        if i == 'O': # i == 'O'면 
+            sum += Correct #  sum에 Correct값을 더해준다.
+            Correct += 1 # 'O'를 만났으므로 correct 값을 1 증가시킨다.
+        else:
+            Correct = 1 # 'O'가 아닌경우('X'만난 경우) Correct값을 1로 변경한다.
+    print(sum)    
