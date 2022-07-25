30d5802ba9a74dde8e303e87ab4f3a75b2eaa63d
runedemonic <runedemonic@naver.com>
2022-07-25 16:13:44 +0900

    algorithm Day 1

--- 3회차/윤효근/20220725/3_OX퀴즈.py
+++ 3회차/윤효근/20220725/3_OX퀴즈.py
@@ -2,3 +2,16 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+number = int(input())
+for i in range(0,number):
+    result = 0
+    a=input().split('X')
+    for i in range(0,len(a)):
+        score = 1
+        plus = a[i].count('O')
+        for i in range(0,plus):
+               result+=score
+               score+=1
+    print(result)
+
