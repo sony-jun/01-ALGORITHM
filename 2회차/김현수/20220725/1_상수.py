dcc0db1b41ef6fa61217b70d7bb8a6215922eb28
maybe-tooday <gjfglgl23@gmail.com>
2022-07-25 14:57:07 +0900

    20220725_알고리즘_실습

--- 2회차/김현수/20220725/1_상수.py
+++ 2회차/김현수/20220725/1_상수.py
@@ -2,3 +2,14 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+#print(int(str(a)[::-1]))
+# 숫자a를 문자열a로 변환하여 리버스하고 다시 숫자로 변환
+a, b = input().split()
+c = int(a[::-1]) 
+d = int(b[::-1])
+
+if c > d :
+    print(c)
+else:
+    print(d)
--- 2회차/김현수/20220725/3_OX퀴즈.py
+++ 2회차/김현수/20220725/3_OX퀴즈.py
@@ -2,3 +2,17 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+T = int(input()) 
+
+for n in range(1,T+1):
+    answer = input() #O.X만 존재
+    score = 0
+    result = 0
+    for c in answer:
+        if c == 'O':
+            score += 1
+            result +=score
+        else:
+            score = 0
+    print(result)
