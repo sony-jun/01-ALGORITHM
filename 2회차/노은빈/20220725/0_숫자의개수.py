bf39b0e1bef9a7a45e4ff09c7679e2699a6cc0fd
rhoeunbin <rhoeb21@gmail.com>
2022-07-25 16:31:03 +0900

    1기_2회차_노은빈_알고리즘문제풀이

--- 2회차/노은빈/20220725/0_숫자의개수.py
+++ 2회차/노은빈/20220725/0_숫자의개수.py
@@ -2,3 +2,11 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+a = int(input())
+b = int(input())
+c = int(input())
+
+m = a* b* c
+result = list(str(m))  #곱한 값을 문자열로 변환 후에 list
+for i in range(10):  #리스트는 0부터 인덱스로 저장 i=0~9
+    print(result.count(str(i)))  #i를 문자열로 바꾸고 count함수를 이용해 문자 개수 세기
--- 2회차/노은빈/20220725/3_OX퀴즈.py
+++ 2회차/노은빈/20220725/3_OX퀴즈.py
@@ -2,3 +2,18 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+t = int(input())
+
+for i in range(t) :  #변수 t만큼 반복
+    lst = list(input())  #input을 리스트 값으로 
+    cnt = 0  #0부터 리스트 안의 값 개수 세기
+    sum = 0  #합계
+
+    for i in lst :  
+        if i == 'O' :  #만약 'o'가 있으면 
+            cnt += 1   # +1
+            sum += cnt  #합계도 개수만큼 더함
+        else :
+            cnt = 0   #'X'라면 0 더함
+    print(sum)
