0cc25ae8cd30678963c747e97cf9ab4c87420211
youjoy123 <limuj333@gmail.com>
2022-07-26 17:56:31 +0900

    220726algo

--- 1회차/임유정/20220725/2_두개뽑아서더하기.py
+++ 1회차/임유정/20220725/2_두개뽑아서더하기.py
@@ -1,8 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
     answer = []
+
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            n1 = numbers[i]
+            n2 = numbers[j]
+
+            sum_= n1 + n2
+            set_.add(sum_)
+
+    answer = sorted(list(set_))
+
     return answer
 
 
-print(solution([2, 1, 3, 4, 1]))
-print(solution([5, 0, 2, 7]))
+# print(solution([2, 1, 3, 4, 1]))
+# print(solution([5, 0, 2, 7]))
