8a93fc53dc79974ee081d1b2da6f75e12ac7f3b5
qlghwp123 <qlghwp123@naver.com>
2022-07-26 14:51:29 +0900

    Add 0726 BOJ PS

---
+++ 3회차/이동근/20220726/28_분해합.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+# 범위 조절 최적화
+min_start = max(1, N - len(str(N)) * 9)
+for i in range(1, N + 1):
+    result = sum(map(int, str(i))) + i
+
+    if N == result:
+        print(i)
+        break
+
+else:
+    print(0)
