058f4706c6adef978b61ef266f258b4461abb678
lol567236 <lol567236@gmail.com>
2022-07-26 17:01:29 +0900

    5시 1번문제만

---
+++ 1회차/정효민/20220726/2953bj.py
@@ -0,0 +1,15 @@
+
+T = int(input())
+for test_case in range(1 , T + 1):
+    
+    result = []
+    result2 = []
+    a , b , c ,d = map(int , input().split())
+    
+    score = [a , b , c ,d]
+    for i in range(len(score)): 
+        result.append(score[i])
+    
+
+         
+
