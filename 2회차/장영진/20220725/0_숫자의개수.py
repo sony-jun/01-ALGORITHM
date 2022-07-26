6064684817442b652b9c00748682bd3928a10ad5
YZYN <yzyn@YZYNui-MacBookPro.local>
2022-07-26 13:57:15 +0900

    0725 add

--- 2회차/장영진/20220725/0_숫자의개수.py
+++ 2회차/장영진/20220725/0_숫자의개수.py
@@ -1,4 +1,8 @@
-# https://www.acmicpc.net/problem/2577
-import sys
+a = int(input())
+b = int(input())
+c = int(input())
 
-sys.stdin = open("0_숫자의개수.txt")
+result = a * b * c
+
+for i in range(10):
+    print(str(result).count(str(i)))
