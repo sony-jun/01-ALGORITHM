de21a16e565debc2f5a5a16107edb43009ba635a
soyolee-dev <soyolee.dev@gmail.com>
2022-07-26 09:50:25 +0900

    20220725 이소영

--- 2회차/이소영/20220725/1_상수.py
+++ 2회차/이소영/20220725/1_상수.py
@@ -2,3 +2,14 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+A, B = input().split()
+
+# 두 수를 역순으로 뒤집는다 
+A_reverse = int(A[::-1])
+B_reverse = int(B[::-1]) 
+
+if A_reverse > B_reverse:
+    print(A_reverse)
+else:
+    print(B_reverse)
