b78eade7a5d8a8579f6ad50e1ea28883570e85c0
tnpfldyd <dkdldhs0322@gmail.com>
2022-07-26 09:40:59 +0900

    2번 코드수정

--- 2회차/이용환/20220725/2_두개뽑아서더하기.py
+++ 2회차/이용환/20220725/2_두개뽑아서더하기.py
@@ -2,12 +2,9 @@
 def solution(numbers):
     answer1 = []
     for i in range(len(numbers)):
-        for j in range(len(numbers)):
-            if i == j:
-                continue
-            else:
-                X = numbers[i]+ numbers[j]
-                answer1.append(X)
+        for j in range(i+1, len(numbers)):
+            X = numbers[i]+ numbers[j]
+            answer1.append(X)
     answer1.sort()
     answer = list(set(answer1))
     return answer
