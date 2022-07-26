f503595532cb050e5086d375fe576191f32039d3
yangu1455 <kunja266@hanmail.net>
2022-07-26 15:28:37 +0900

    2846번 오르막길

--- 2회차/황지선/20220726/2846 오르막길.py
+++ 2회차/황지선/20220726/2846 오르막길.py
@@ -19,10 +19,12 @@ res = []
 h = 0
 
 for i in range(1, N):
+
     if Pi[i] > Pi[i-1]:
         h+= Pi[i] - Pi[i-1]
         if i == N-1:
             res.append(h)
+    
     else:
         res.append(h)
         h = 0
