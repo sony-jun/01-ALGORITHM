468802cf0f40bd96f92bb422093b55051f65ccfa
Bonbon315 <n202u123@gmail.com>
2022-07-26 15:17:38 +0900

    20220726 1기 2회차 조본희 완료

--- 2회차/조본희/20220726/1_분해합.py
+++ 2회차/조본희/20220726/1_분해합.py
@@ -2,9 +2,11 @@ N = int(input())
 
 result = 0
 for i in range(1, N+1):
+    #없는경우
     if i == N:
         print(0)
         break
+    
     test_case = list(map(int, str(i)))
     result = i + sum(test_case)
     if result == N:
--- 2회차/조본희/20220726/3_나는 요리사다.py
+++ 2회차/조본희/20220726/3_나는 요리사다.py
@@ -4,5 +4,7 @@ sys.stdin = open("3_input.txt")
 cooks = []
 
 for i in range(5):
+    #입력받은 동시에 총점수 저장
     cooks.append(sum(map(int, input().split())))
+#index+1로 참가자번호, 해당참가자 점수 출력
 print(cooks.index(max(cooks))+1, max(cooks))
