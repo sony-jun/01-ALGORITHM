3ec437b0a229ed049608918a926019e238ead2b3
yoosoonil <y2k7654@gmail.com>
2022-07-26 17:58:39 +0900

    3회차_유순일_알고리즘

--- 3회차/유순일/20220726/27_2609_최대공약수.py
+++ 3회차/유순일/20220726/27_2609_최대공약수.py
@@ -5,5 +5,5 @@ for i in range(1, min(a,b) + 1):    # (a, b) = (24, 18) -> range(1, 19)
     if a % i == 0 and b % i == 0:   # 24 % (1~19) 그리고 18 % (1~19) = 0 이라면
         tmp.append(i)               # tmp에 추가해.
 
-print(max(tmp)) # temp 중에 최대수 -> 최대공약수
+print(max(tmp))                      # temp 중에 최대수 -> 최대공약수
 print(max(tmp) * (a // max(tmp)) * (b // max(tmp))) # 최대공약수 * a//최대공약수 * b//최대공약수
--- 3회차/유순일/20220726/30_2953_나는요리사.py
+++ 3회차/유순일/20220726/30_2953_나는요리사.py
@@ -4,7 +4,7 @@ sys.stding = open ('2953.txt', 'r')
 
 score = []
 
-for _ in range(5) :
+for i in range(5) :
   score.append(sum(map(int, input().split())))
 
 print(score.index(max(score))+1, max(score))
