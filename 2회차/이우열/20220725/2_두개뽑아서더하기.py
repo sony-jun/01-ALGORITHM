b303ea2bfb5b59ff2f4fa6a6379616bc4bc583a0
w00ye0l <dnduf158@gmail.com>
2022-07-26 13:33:31 +0900

    Revert "1기 2회차 이우열 20220725"
    This reverts commit c7d0e6a2540a682a3e1ec7f22acee6d3ced597a6.

--- 2회차/이우열/20220725/2_두개뽑아서더하기.py
+++ 2회차/이우열/20220725/2_두개뽑아서더하기.py
@@ -4,7 +4,7 @@ def solution(numbers):
 
     for i in range(len(numbers)):
         # 이중 for문을 사용하여 같은 인덱스가 아닌 수끼리 더해주고
-        for j in range(i + 1, len(numbers)):
+        for j in range(i+1, len(numbers)):
             answer.append(numbers[i] + numbers[j])  # 배열에 추가한다
 
     # set로 중복을 없앤 뒤 리스트로 형 변환하여 정렬한다
