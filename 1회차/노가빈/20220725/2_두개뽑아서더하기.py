2fd34c6d042d301e47f4b86506c825668c3442d7
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2022-07-26 19:12:18 +0900

    0726 commit

--- 1회차/노가빈/20220725/2_두개뽑아서더하기.py
+++ 1회차/노가빈/20220725/2_두개뽑아서더하기.py
@@ -1,16 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
-    answer = []
     numList = []
-    for i in numbers:
-        print(i)
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j :
+                n = numbers[i] + numbers[j]
+                numList.append(n)
+    numList = sorted(set(numList))     
     
-
-
-
-
-
-    return answer
+    return numList
 
 
 print(solution([2, 1, 3, 4, 1]))
