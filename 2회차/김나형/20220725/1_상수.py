94ad1092e9baa6e050ba98f808204d69bf7bdfc6
na_hyeong <skgud9511@gmail.com>
2022-07-26 10:02:04 +0900

    2회차_김나형_실습_20220725

--- 2회차/김나형/20220725/1_상수.py
+++ 2회차/김나형/20220725/1_상수.py
@@ -2,3 +2,19 @@
 import sys
 
 sys.stdin = open("1_상수.txt")
+
+a, b = map(str, input().split())
+
+a = a[::-1] # 정수를 리스트로 형변환후 슬라이싱으로 뒤집기
+b = b[::-1]
+
+list_1 = ''.join(a) # 리스트형식을 join 함수로 연결하여 출력
+list_2 = ''.join(b) 
+
+
+if list_1> list_2:
+    print(list_1)
+else:
+    print(list_2)
+
+
--- 2회차/김나형/20220725/2_두개뽑아서더하기.py
+++ 2회차/김나형/20220725/2_두개뽑아서더하기.py
@@ -2,7 +2,16 @@
 def solution(numbers):
     answer = []
     return answer
-
+      
+def solution(numbers):
+    answer = list()
+    
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if not numbers[i] + numbers[j] in answer:
+                answer.append(numbers[i] + numbers[j])
+        
+    return sorted(answer)
 
 print(solution([2, 1, 3, 4, 1]))
 print(solution([5, 0, 2, 7]))
