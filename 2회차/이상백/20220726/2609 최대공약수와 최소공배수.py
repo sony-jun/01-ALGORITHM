82de8efddde7509fd43b481145d7819e876eb880
lsb0709 <dltkdqor0709@gmail.com>
2022-07-26 17:58:09 +0900

    0726

---
+++ 2회차/이상백/20220726/2609 최대공약수와 최소공배수.py
@@ -0,0 +1,44 @@
+# 문제
+# 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에는 두 개의 자연수가 주어진다. 이 둘은 10,000이하의 자연수이며 사이에 한 칸의 공백이 주어진다.
+
+# 출력
+# 첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
+
+
+a, b = map(int, input().split())
+
+for i in range(min(a, b),0, -1):
+    if a % i == 0 and b % i == 0:
+        print(i) # 최대공약수
+        break
+
+for i in range(max(a, b), (a * b) + 1):
+    if i % a == 0 and i % b == 0 :
+        print(i) # 최소공배수
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from math import gcd, lcm
+
+
+# a, b = map(int, input().split())
+
+# print(gcd(a, b)) # 최대공약수
+
+# print(lcm(a, b)) # 최소공배수
