109d5e363f47a655fc14e0e510ea282e4a0cc1bd
star2871 <star2871@gmail.com>
2022-07-26 20:53:37 +0900

    29번 백준 문제완료

--- 1회차/송희수/20220725/16_합.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.stdin = open("16_합.txt")
-
-n = int(input())
-
-for i in range(1,n+1):
-    i = str    
-    print(i)
--- 1회차/송희수/20220726/29_오르막길.py
+++ 1회차/송희수/20220726/29_오르막길.py
@@ -1,5 +1,17 @@
 import sys
 
 sys.stdin = open("29_오르막길.txt")
-T =int(input())
-for i in range(T):
+N =int(input())
+road = list(map(int, sys.stdin.readline().split()))
+hill = 0
+tmp = []
+
+for i in range(N - 1):
+    if road[i] < road[i + 1]:
+        hill += (road[i + 1] - road[i])
+    if road[i] >= road[i + 1]:
+        tmp.append(hill)
+        hill = 0
+tmp.append(hill)
+
+print(max(tmp))
