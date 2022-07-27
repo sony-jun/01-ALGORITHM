505c0cb0f59b71d0cd064c3595a7f3da0c3ca66b
koo1996 <koodo1996@gmail.com>
2022-07-27 10:03:28 +0900

    알고리즘_2 실습완료

--- 2회차/구도회/20220726/29번 오르막길(BOJ2846).py
+++ 2회차/구도회/20220726/29번 오르막길(BOJ2846).py
@@ -1,14 +1,12 @@
 N = int(input())
 Size_ = list(map(int,input().split()))
-result = []
 cnt = 0
-for test_1 in range(1,N):
-    if Size_[test_1] > Size_[test_1 - 1]:
-        X = Size_[test_1] - Size_[test_1 - 1]
-        cnt = cnt + X
-        if test_1 == N:
-            result.append(cnt)
+max_sum = 0
+for test_1 in range(1,len(Size_)):
+    if Size_[test_1] > Size_[test_1 - 1]: 
+        cnt = cnt + Size_[test_1] - Size_[test_1 - 1]
+        max_sum = max(max_sum, cnt) 
+
     else:
-        cnt = 0
-        result.append(cnt)
-print(result)
+        cnt = 0     
+print(max_sum)
