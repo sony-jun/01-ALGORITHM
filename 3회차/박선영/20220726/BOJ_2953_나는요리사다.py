28418f026b8891f81c56382924d72318828e8b42
psun0610 <psy010610@naver.com>
2022-07-27 14:27:13 +0900

    코드 깔끔하게 정리함

--- 3회차/박선영/20220726/BOJ_2953_나는요리사다.py
+++ 3회차/박선영/20220726/BOJ_2953_나는요리사다.py
@@ -1,9 +1,9 @@
-high_score = 0
+best_score = 0
 winner = 0
 for i in range(1, 6):
     chef = list(map(int, input().split()))
     score = sum(chef)
-    if high_score < score:
-        high_score = score
+    if best_score < score:
+        best_score = score
         winner = i
-print(winner, high_score)
+print(winner, best_score)
