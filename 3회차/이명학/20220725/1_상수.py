b66fd6bb75ecaa9ec4808581a4a8d9ef68a14d4d
Myeonghak Lee <mhmh779@naver.com>
2022-07-27 17:00:37 +0900

    제출

--- 3회차/이명학/20220725/1_상수.py
+++ 3회차/이명학/20220725/1_상수.py
@@ -5,13 +5,18 @@ sys.stdin = open("1_상수.txt")
 
 a, b = input().split()
 
-a_list = []
-b_list = []
+a_list = []  # 734
+b_list = []  # 893
+
+# 입력값을 리스트에 삽입
 for i in a:
     a_list.append(i)
 for j in b:
     b_list.append(j)
+# a_list = ['7','3','4']
+# b_list = ['8','9','3']
 
+# 임시값을 할당해서 각 리스트 0과 2 인덱스 값을 교환
 temp = a_list[0]
 a_list[0] = a_list[2]
 a_list[2] = temp
