9eff999dae1e540b51142272e829f6bf9ba720e0
dongwonbaek <bmrio100@gmail.com>
2022-07-27 21:24:07 +0900

    7/27 문제풀이 추가

--- 3회차/백동원/20220725/3_OX퀴즈.py
+++ 3회차/백동원/20220725/3_OX퀴즈.py
@@ -5,7 +5,7 @@ sys.stdin = open("3_OX퀴즈.txt")
 
 T = int(input())                
 for i in range(1, T + 1):       
-    test_case = list(input())  # 입력받은 OX 문제를 하나하나 쪼개어 리스트에 저장한다.
+    test_case = list(input())   # 입력받은 OX 문제를 하나하나 쪼개어 리스트에 저장한다.
     result = 0                  # 결과값은 매 문제마다 다르니 시작시에 초기화할 수 있게 한다.
     point = 1                   # 포인트 시작값은 1이다.
     for OX_judge in test_case:  # 리스트로 쪼갠 OX문제에서 처음부터 하나씩 순회한다.
--- 3회차/백동원/20220727/7568.py
+++ 3회차/백동원/20220727/7568.py
@@ -1,24 +1,23 @@
 T = int(input())
 number_list = []
-dungchi_over =[]
+dungchi =[]
 for a in range(T):
-    number_list.append(input().split())
+    number_list.append(list(map(int, input().split()))) # 리스트 내에 리스트로 받는다.
 
 for b in number_list:
     cnt = 0
-    for c in number_list:
-        if b[0] > c[0] and b[1] > c[1]:
+    for c in number_list:                   # 이중 for 문으로 
+        if b[0] < c[0] and b[1] < c[1]:     # 나보다 덩치가 작은 사람 수를 카운트
             cnt += 1
-    dungchi_over.append(cnt)
+    dungchi.append(cnt + 1)                 # 나의 등수는 나보다 덩치가 작은 사람 수 + 1 이므로
+                                            # 계산해서 리스트에 추가
+for d in dungchi:                           # 순서대로 출력하되       
+    print(d, end = ' ')                     # 띄어쓰기 대신 공백으로
 
-tier_list = []
-
-for d in dungchi_over:
-    tier_cnt = 0
-    for e in dungchi_over:
-        if d >= e:
-            tier_cnt += 1
-    tier_list.append(len(dungchi_over) - tier_cnt + 1)
-
-for h in tier_list:
-    print(h, end = ' ')
+# tier_list = []
+# for d in dungchi_over:
+#     tier_cnt = 0
+#     for e in dungchi_over:
+#         if d < e:
+#             tier_cnt += 1
+#     tier_list.append(tier_cnt + 1)
