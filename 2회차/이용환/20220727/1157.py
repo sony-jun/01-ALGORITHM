297592bd39c2665b043578a6c603e2e80e357843
tnpfldyd <dkdldhs0322@gmail.com>
2022-07-27 17:48:17 +0900

    7.27 수정

--- 2회차/이용환/20220727/1157.py
+++ 2회차/이용환/20220727/1157.py
@@ -1,15 +1,11 @@
 text_ = input()
-result = []
-for i in text_:
-    if ord(i) > 96:
-        c = chr(ord(i)-32)
-        result.append(c)
-    else:
-        result.append(i)
+a = text_.upper()
+result = list(a)
 cnt = {}
 for j in result:
     if j in cnt:
         cnt[j] += 1
     else:
         cnt[j] = 1
+
 print('?') if len([k for k,v in cnt.items() if max(cnt.values()) == v]) > 1 else print(max(cnt, key=cnt.get))
