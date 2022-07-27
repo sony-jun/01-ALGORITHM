20e79d7e3c01e26a64f9bb3a5f9dde9416efb97d
Taeho Park <qkrxogh7@gmail.com>
2022-07-27 23:48:17 +0900

    add 2941

--- 2회차/박태호/20220727/2941.py
+++ 2회차/박태호/20220727/2941.py
@@ -1,1 +1,11 @@
 #크로아티아 알파벳
+
+croa = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+cnt = 0
+w = input() # ljes=njak
+len_w = len(w) # 전체 길이에서 크로 뺄거임
+#일단 croa 순회
+for i in croa:
+    cnt += w.count(i) # 해당문자 개수를 cnt에 저장  
+         
+print(len_w - cnt)
