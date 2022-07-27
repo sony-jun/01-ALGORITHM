cb899fef7148b6c35b2f726d2440f81053acd8df
tmsnvl95 <tmsnvl95@naver.com>
2022-07-27 09:08:21 +0900

    0726update

---
+++ 1회차/이종은/20220726/2231.py
@@ -0,0 +1,14 @@
+
+n = int(input())
+
+result = 0
+for i in range(1, n+1):
+    a = list(map(int, str(i)))
+    result = i + sum(a)
+    
+    #  1~ 198 까지 리스트로 출력
+    # print(result) 1~216 두개씩 건너뛰어서 출력
+    # print(i) 1~216
+    if result == n: #n값 216과 result 값 같을 떄 i값 출력 
+        print(i)
+        break
