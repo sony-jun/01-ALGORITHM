7331d32d837bb03400073d20c8f70474b28f4ab9
yangu1455 <kunja266@hanmail.net>
2022-07-27 17:38:43 +0900

    2851번 슈퍼 마리오

--- 2회차/황지선/20220727/2851.py
+++ 2회차/황지선/20220727/2851.py
@@ -5,21 +5,27 @@ score_dict = {}
 a = 0
 b = 0
 
-for i in range(1, 11):
+for i in range(1, 11): # 10개의 버섯 먹어서 점수의 합을 딕셔너리에 넣기
     s = int(input())
     score += s
     score_dict[i] = score
 
-for j in range(1, len(score_dict)) :
-    if score_dict[j] >= 100:
-        a = score_dict[j]
+for j in range(1, len(score_dict)) : # 딕셔너리 길이만큼 반복문
+    if score_dict[j] >= 100:  # 100보다 크거나 같으면 정지해서 해당 인덱스의 값과 그 이전 값을 a, b에 저장
+        a = score_dict[j] 
         b = score_dict[j-1]
         break
 
-if abs(a - 100) != 0:
-    if abs(a - 100) <= abs(b - 100):
+if abs(a - 100) != 0: # 절댓값 함수를 써서 a에서 100을 뺀 수가 100과 같지 않으면 
+    # 100을 뺀 절댓값 크기 비교해서 출력
+    if abs(a - 100) <= abs(b - 100): 
         print(a)
-    elif abs(a - 100) > abs(b - 100):
+    elif abs(a - 100) > abs(b - 100): 
         print(b)
-else:
+else: # 절댓값이 100이면 a 출력
     print(a)
+
+
+# 테스트 케이스만 돌려보고 다른 예외는 생각치도 못했습니다. 
+# 합이 100에 닿지 않을때의 경우를 고려해서 다시 코드를 짜봐야겠습니다. 
+# 그리고… 딕셔너리는 느리다!
