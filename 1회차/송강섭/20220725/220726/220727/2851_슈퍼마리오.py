5e1a918abb0e907e4b1aa279dc04a3c38f6fb160
1c0332zz <1c0332zz@gmail.com>
2022-07-27 18:06:21 +0900

    1회차 송강섭

---
+++ 1회차/송강섭/20220725/220726/220727/2851_슈퍼마리오.py
@@ -0,0 +1,3 @@
+import sys
+
+sys.stdin = open("2851_슈퍼마리오.txt")
--- 1회차/송강섭/20220725/220726/2609_최대공약수와 최소공배수.py
+++ 1회차/송강섭/20220725/220726/2609_최대공약수와 최소공배수.py
@@ -1,16 +1,12 @@
 # https://www.acmicpc.net/problem/2609
 A, B = map(int, input().split())
-a, b = A, B
+a, b = A, B # a,b 로 정의를 다시 하는 이유는 A,B가 최소공배수 계산에 필요하기 때문
 
-while b != 0: # 
-    a = a % b
-    a, b = b, a
+while b != 0: # b가 0이 되면 자동으로 와일문이 종료되도록
+    a = a % b # a는 a % b의 몫. 정수이다.
+    a, b = b, a # 최소공배수가 나올떄까지 수를 뒤집어서 계속 a%b를 해준다.
 print(a) # 최대공약수
 print(A*B//a) # 최소공배수 
-
-
-
-
 
 # for i in range(min(A, B), 0, -1):
 #     if A % i == 0 and B % i == 0:
