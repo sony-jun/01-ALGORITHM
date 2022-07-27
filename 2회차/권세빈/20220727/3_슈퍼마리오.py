d8dfcc30590d4d12dd3344500faafe912cda1ff0
sebin kwon <sbkwon98@gmail.com>
2022-07-27 22:48:38 +0900

    2회차 권세빈 0727 실습 완료

--- 2회차/권세빈/20220727/3_슈퍼마리오.py
+++ 2회차/권세빈/20220727/3_슈퍼마리오.py
@@ -3,18 +3,12 @@ import sys
 
 sys.stdin = open("3.txt", "r")
 
-n = []
 sum_ = 0
-for t in range(10):
-    n.append(int(input()))
-
-for i in n:
-    sum_ += i
-    if sum_ >= 100:        
-        if sum_ - 100 > 100 - (sum_ - i):
-            sum -= i
+for i in range(10):     # 10개 버섯들
+    n = int(input())    # 점수 입력값 받기
+    sum_ += n           # 점수 합치기
+    if sum_ >= 100:     # 만약 100보다 같거나 크다면
+        if sum_ - 100 > 100 - (sum_ - n):   # 100을 넘겼을 때 값을 더한 값과 값을 더하지 않은 값을 비교
+            sum_ -= n                       # 차이가 더 적은 쪽을 출력한다.
         break
-print(sum_)
-
-
-    
+print(sum_)             # 100보다 작을 땐 if문 실행 안하고 그냥 바로 print됨   
--- 2회차/권세빈/20220727/4_덩치.py
+++ 2회차/권세빈/20220727/4_덩치.py
@@ -1,1 +1,19 @@
 # https://www.acmicpc.net/problem/7568
+
+import sys
+sys.stdin = open("4.txt", "r")
+
+n = int(input())
+person = []
+
+for i in range(n):
+    w , h = map(int,input().split())
+    person.append([w,h])
+
+for j in person:
+    rank = 1
+    for k in person:
+        if j[0] < k[0] and j[1] < k[1]:
+            rank += 1 
+                      
+    print(rank, end=' ')
