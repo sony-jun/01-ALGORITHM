4975abaf291e0afe5b4eabc242aa03d4f5d316fd
1_1_LJS <jeongseob.korea@gmail.com>
2022-07-28 23:52:20 +0900

    finished 0728 qn

---
+++ 1회차/이정섭/20220728/Dial.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+# it takes 2 sec to dial #1
+# the dialing time increases by 1 sec by ascending numbers on dial
+#   e.g. dial 1 = 2 sec, dial 2 = 3 sec, dial 9 = 10 sec and dial 0 = 11 sec
+# Every time when you press number, the dial will be reset and should start from the beginning
+
