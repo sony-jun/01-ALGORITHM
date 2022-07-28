a98825964cd366e8ba4d2393bea040458a901178
leesc8408 <leesc8408@gmail.com>
2022-07-28 10:03:43 +0900

    0727_강사님 풀이를 마지막으로 완료

--- 1회차/이순철/20220727/34_슈퍼 마리오.py
+++ 1회차/이순철/20220727/34_슈퍼 마리오.py
@@ -1,19 +1,20 @@
 import sys
 
 sys.stdin = open("34_슈퍼 마리오.txt")
-score_list = []
-score = 0
 
-for i in range(10):
-    score_list.append(int(input()))
+# score_list = []
+# score = 0
 
-for i in score_list:
-    score += i
-    if score >= 100:
-        if score - 100 > 100 - (score - i):
-            score -= i
-        break      
-print(score)    
+# for i in range(10):
+#     score_list.append(int(input()))
+
+# for i in score_list:
+#     score += i
+#     if score >= 100:
+#         if score - 100 > 100 - (score - i):
+#             score -= i
+#         break      
+# print(score)    
 
 
 score_list = []
@@ -27,5 +28,7 @@ for i in score_list:
     if score >= 100:
         prev_score = score - i 
         out = prev_score if score - 100 >= 100 - prev_score else score
-        break      
+        break
+    else:
+        out = score
 print(out) 
