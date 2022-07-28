a9d026a4197eeb4eeb87e7d3815c9f631e109124
yangu1455 <kunja266@hanmail.net>
2022-07-28 15:49:03 +0900

    3052번 나머지

--- 2회차/황지선/20220728/3052.py
+++ 2회차/황지선/20220728/3052.py
@@ -3,9 +3,13 @@
 n= []
 
 for i in range(10):
+    # 수를 10번 입력받는다.
     a= int(input())
+    # 입력받은 수를 42로 나눈 나머지를 b에 저장한다.
     b= a % 42
+    # 리스트 n에 b를 넣는다.
     n.append(b)
 
+# 리스트를 set으로 중복을 제거한 후 길이를 출력한다.
 x= set(n)
 print(len(x))
