70b639ebcd9e9894399e8ccf6c8b2679dd20bbb1
mhd329 <mhd329@gmail.com>
2022-07-29 02:45:56 +0900

    1기 3회차 문현동 제출

--- 3회차/문현동/20220727/4_덩치.py
+++ 3회차/문현동/20220727/4_덩치.py
@@ -1,4 +1,15 @@
 import sys
 sys.stdin = open("4_덩치.txt", 'r')
 
-T = int(input())
+wh = []
+
+N = int(input())
+for people in range(N):
+    wh.append(list(map(int, input().split())))
+
+for i in wh:
+    rank = 1
+    for j in wh:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end = ' ')
