971b81c4b1b619b9ab46176f17954990d8ccc263
dahlia <wlstnrdahlia@gmail.com>
2022-07-28 09:47:14 +0900

    Add : 음양, 슈퍼마리오, 덩치 풀이 추가

--- 3회차/류진숙/20220727/단어공부.py
+++ 3회차/류진숙/20220727/단어공부.py
@@ -1,23 +1,10 @@
 # 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 
 # 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
 
-# 우선, 문자열에서 알파벳 소문자 대문자 같은걸로 인식하는지 확인해본다.
-
-# word = 'zZa'
-
-# cnt = 0
-# for str in word:
-#     if str == 'z':
-#         cnt += 1
-
-# print(cnt)
-
-# 같은 걸로 인식 안함 - 다른 방법 고안
 # 우선 전부 다 대문자로 바꾼다음에 대문자 겹치는 문자를 튀어 나오게 하자!
 
 
-word = input()
-word = word.upper() # 대문자로 바꿔 준다
+word = input().upper() # 대문자로 바꿔 준다
 
 str_dict = {}
 for str in word:
@@ -26,20 +13,19 @@ for str in word:
     else:
         str_dict[str] += 1
 
-v_list = list(str_dict.values())
-v_list = list(map(int, v_list))
-
-final_list = []
+k_list = []
+max_v = max(str_dict.values())
 for key, value in str_dict.items():
-    if value == max(v_list):
-        final_list.append(key)
-
-if len(final_list) > 1:
+    if value == max_v:
+        k_list.append(key)
+    
+if len(k_list) == 1:
+    print(k_list[0])
+elif len(k_list) > 1:
     print('?')
-elif len(final_list) == 1:
-    print(final_list[0])
+# 정답 256 ms 
 
-# 정답 264 ms
+
 
 # 구글링 검색     
 word = input().upper()
@@ -54,6 +40,4 @@ if cnt.count(max(cnt)) >= 2:
     print('?')
 else:
     print(word_list[(cnt.index(max(cnt)))])
-
 # 정답 108 ms
-
