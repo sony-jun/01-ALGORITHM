ca42379de9cf28d1a9647d1f08536eaf624bf889
code-sum <yisugyeong@gmail.com>
2022-07-29 01:26:54 +0900

    [2회차_이수경] 1-2일차 필수문제 풀이완료

--- 2회차/이수경/20220725/3_OX퀴즈.py
+++ 2회차/이수경/20220725/3_OX퀴즈.py
@@ -3,3 +3,18 @@ import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
 
+# 첫째 줄에 테스트 케이스의 개수
+test_case = int(input())
+
+for _ in range(test_case):
+    OX = input()
+    score = 0
+    count = 0
+    for i in range(len(OX)):
+        if OX[i] == "O":
+            count += 1
+            score += count
+        if OX[i] == "X":
+            count = 0
+    print(score)
+
