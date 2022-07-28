2eadc830b1e4dd95b2ba2b992389c4e1a0c5a2fa
ererink <yelkim0210@gmail.com>
2022-07-28 20:26:33 +0900

    2회차 김예린 수정본 제출

--- 2회차/김예린/20220728/5622.py
+++ 2회차/김예린/20220728/5622.py
@@ -1,4 +1,15 @@
 # 다이얼
 
-alpabet_list = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+# 1 - 2초, 2 - 3초
+
+alpa_list = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
 word = input()
+time = 0
+
+for i in word:
+    for j in alpa_list:
+        if i in j :
+            time += alpa_list.index(j) + 3   # alpa_list에서의 0의 기본 시간값은 3이다. 
+
+print(time)
+
