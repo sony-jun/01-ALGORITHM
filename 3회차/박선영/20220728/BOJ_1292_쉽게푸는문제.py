891047ed2c5c11eceacbd8894ac96e593dc6d3f6
psun0610 <psy010610@naver.com>
2022-07-29 09:37:32 +0900

    코드 방법 2 추가

--- 3회차/박선영/20220728/BOJ_1292_쉽게푸는문제.py
+++ 3회차/박선영/20220728/BOJ_1292_쉽게푸는문제.py
@@ -5,3 +5,15 @@ for i in range(1, B):
         data.append(i)
 
 print(sum(data[A:B+1]))
+
+# 강사님 코드
+data = []
+N = 1
+
+# 수열의 길이가 B보다 작을 때까지 수열에 숫자 추가
+while len(data) < B:
+    # N의 크기만큼 수열 리스트에 N을 추가한다.
+    for _ in range(N):
+        data.append(N)
+    N += 1
+print(sum(data))
--- 3회차/박선영/20220728/BOJ_3052_나머지.py
+++ 3회차/박선영/20220728/BOJ_3052_나머지.py
@@ -1,4 +1,4 @@
 list_ = []
 for i in range(10):
     list_.append(int(input()) % 42)
-print(10 - (len(list_) - len(list(set(list_)))))
+print(len(list(set(list_))))
