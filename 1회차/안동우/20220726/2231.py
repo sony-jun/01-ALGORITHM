5756742f53c24e66a89826f7a7c87cfc51ae39c8
an dong woo <akskwha@naver.com>
2022-07-30 23:28:27 +0900

    220730문제풀이

--- 1회차/안동우/20220726/2231.py
+++ 1회차/안동우/20220726/2231.py
@@ -1,26 +1,2 @@
 
-a,b= map(int, input().split())
 
-#배수 약수 구하고
-#공약수 공배 구하고
-#최대공약수 최소공배수구하고
-#출력
-a=a
-b=b
-measure=[] #공약수
-Drainage1=[]#공배수
-Drainage2=[]
-
-for i in  range(1,b+1):
-    if a%i== 0 and b%i==0:
-        measure.append(i)
-        measure.sort(i)
-print(measure) 
-for j in range(1,a+1):
-    if j*a:
-        Drainage1.append(j*a)
-    if j*b:
-        Drainage2.append(j*b)
-Drainage3=list(set(Drainage1).intersection(Drainage2))
-Drainage3.sort()
-print(Drainage3[0])
--- 1회차/안동우/20220727/7568.py
+++ 1회차/안동우/20220727/7568.py
@@ -1,24 +1,14 @@
 
-from re import I
 import sys
 sys.stdin = open("7568.txt", "r")
 
 N=int(input())
+a=""
+b=[]
 
-a=[]
-b=''
 for i in range(1,N+1):
     x,y=map(int,(input()).split())
-    a= round(x+y/2,-1)#키와 몸무게 평균 일자리까지 반올림
-    if a[0] ==150.0:
-        b=2
-    elif a[1] ==150.0:
-        b=2
-    elif a[2] ==180.0:
-        b=1
-    elif a[3] ==150.0:
-        b=2
-    elif a[4] ==120.0:
-        b=3
-
-        #딕셔너리 
+    a=str(round(x+y/2,-1))#키와 몸무게 평균 일자리까지 반올림
+    # A,B,C,D,F=map(int,(input()).split())
+    print(a)
+    
