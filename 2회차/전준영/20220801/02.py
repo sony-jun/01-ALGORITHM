cfb0b1cf1f5358560a49ef53276b7508022f5c14
slugjeon <youngman0515@naver.com>
2022-08-01 17:44:45 +0900

    08/01

---
+++ 2회차/전준영/20220801/02.py
@@ -0,0 +1,20 @@
+answer=[]
+T=int(input())
+for i in range(T):
+    str0=input()
+    cnt=0
+    bo=1
+    for k in str0:
+        if(k=='('):
+            cnt+=1
+        else:
+            cnt-=1
+        if(cnt<0):
+            bo=0
+            break
+    if(bo==1 and cnt==0):
+        answer.append("YES")
+    else:
+        answer.append("No")
+for k in answer:
+    print(k)
