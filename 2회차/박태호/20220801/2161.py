de2a50d9616f44a3b2eed012db2f611ad850579e
Taeho Park <qkrxogh7@gmail.com>
2022-08-01 23:39:53 +0900

    괄호,자료구조 수정

--- 2회차/박태호/20220801/2161.py
+++ 2회차/박태호/20220801/2161.py
@@ -4,8 +4,6 @@
 # 7
 # 1 3 5 7 4 2 6
 from collections import deque
- 
-
 
 N = int(input())#7
 
@@ -17,12 +15,12 @@ que = deque(cards)
 
 remove_card = []
 # print(que.popleft(),que)
-while len(que) > 1:
+while len(que) > 1:     #
     s_num = 0
     
     remove_card.append(que.popleft()) # 첫번째를 빼고
     
     #두번째는 빼서 뒤로
-    s_num=(que.popleft()) # 이거는 2번째 수 컨트롤
+    s_num = (que.popleft()) # 이거는 2번째 수 컨트롤
     que.append(s_num) # 뒤로 넣음
 print(*remove_card,*que)
