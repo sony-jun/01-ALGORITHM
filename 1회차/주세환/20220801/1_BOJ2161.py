1484b6a463ffca0677e29ca5918f6455a6ccc6a8
JOO-10000-Zi <j4615545@gmail.com>
2022-08-01 17:08:56 +0900

    수정

--- 1회차/주세환/20220801/1_BOJ2161.py
+++ 1회차/주세환/20220801/1_BOJ2161.py
@@ -3,7 +3,7 @@ from collections import deque
 N = int(input())
 
 card_list = []
-card_list = deque(card_list)
+card_list = deque(card_list)    # deque로 현병환
 for i in range(1, N+1):
     card_list.append(i)
 # print(card_list.popleft())
