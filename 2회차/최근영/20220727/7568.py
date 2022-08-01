43f3ea9ca685765149cb5f98f43afea44fb52dcd
choikeunyoung <ought119@gmail.com>
2022-08-01 23:17:27 +0900

    20220801

--- 2회차/최근영/20220727/7568.py
+++ 2회차/최근영/20220727/7568.py
@@ -3,13 +3,12 @@ all_list=[]
 cnt_list=[]
 for i in range(1,N+1):
     all_list.append(list(map(int,input().split())))
-    cnt_list.append(1)
-print(cnt_list)
-for i in range(0,len(all_list)-1):
-    for j in range(1,len(all_list)):
-        if all_list[i][0] > all_list[j][0] and all_list[i][1] > all_list[j][1]:
-            cnt_list[i] += 1
-        else:
-            continue
+    cnt_list.append(0)
 
-print(cnt_list)
+for i in range(0,len(all_list)):
+    for j in range(0,len(all_list)):
+        if all_list[i][0] > all_list[j][0] and all_list[i][1] > all_list[j][1]:
+            cnt_list[j] += 1
+
+for k in cnt_list:
+    print(k+1,end=' ')
