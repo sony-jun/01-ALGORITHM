55c99379f4119b8f950e982a3f7521c179744608
DrugCoding <lhs940308@gmail.com>
2022-08-02 22:01:31 +0900

    8월2일 화요일 필수문제풀이(다시풀어보기 2문제못품)

---
+++ 1회차/이현성/8월2일 화요일/1269 대칭차집합.py
@@ -0,0 +1,3 @@
+stack = [1, 2, 3, 4]
+
+print(stack.pop())
---
+++ 1회차/이현성/8월2일 화요일/5063 TGN.py
@@ -0,0 +1,10 @@
+s = int(input())
+
+for i in range(s):
+    a, b, c = map(int, input().split())
+    if a == b - c:
+        print('does not matter')
+    elif a > b - c:
+        print('do not advertise')
+    else:
+        print('advertise')
