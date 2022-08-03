e9f288d50d844458074ab3c842c41a2cdffd3183
sebin kwon <sbkwon98@gmail.com>
2022-08-03 23:49:16 +0900

    2회차 권세빈 0803 실습 완료

--- 2회차/권세빈/20220803/2_2차원배열의합.py
+++ 2회차/권세빈/20220803/2_2차원배열의합.py
@@ -10,12 +10,27 @@ input = sys.stdin.readline
 n, m = map(int,input().split())
 a = [list(map(int,input().split())) for _ in range(n)]
 k = int(input())
-sum_ = []
 
-for i in range(k):
+for b in range(k):
     i, j, x, y = map(int,input().split())
-    
+    sum_ = 0
+    for c in range(i-1,x):
+        for d in range(j-1,y):
+            sum_ += a[c][d]
+    print(sum_)
 
+#? 동적 계획법
+# n, m = map(int, input().split())
+# arr = [list(map(int, input().split())) for _ in range(n)]
+# memo = [[0] * (m+1) for _ in range(n+1)]
 
-
-
+ 
+# for i in range(1, n+1):
+#     for j in range(1, m+1):
+#         memo[i][j] = arr[i-1][j-1] + memo[i][j-1] + memo[i-1][j] - memo[i-1][j-1]
+#         print(  memo[i][j-1])
+ 
+# k = int(input())
+# for _ in range(k):
+#     i, j, x, y = map(int, input().split())
+#     print(memo[x][y] - memo[i-1][y] - memo[x][j-1] + memo[i-1][j-1])
