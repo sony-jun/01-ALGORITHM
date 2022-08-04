61acc65da295fec79b3f835fef94db6305fa281c
wxxk <dwde22@gmail.com>
2022-08-05 01:36:53 +0900

    0805 성장 문제풀이

--- 1회차/이상욱/20220804/2693. N번째 큰 수/BOJ_2693.py
+++ 1회차/이상욱/20220804/2693. N번째 큰 수/BOJ_2693.py
@@ -1,25 +1,10 @@
 import sys
 sys.stdin = open('2693.txt')
 
+N = 3
 T = int(input())
 
 for i in range(1, T+1):
-    N = 3
-    A = list(map(int, input().split()))
-
-    while N != 1:
-        A[A.index(max(A))] = 0
-        N -= 1
-    print(max(A))
-
-
-'''
-T = int(input())
-
-for i in range(1, T+1):
-    N = 3
-    A = list(map(int, input().split()))
-    A.sort()
-    print(A[-N])
-'''
-
+    numbers = list(map(int, input().split()))
+    numbers.sort()
+    print(numbers[-N])
