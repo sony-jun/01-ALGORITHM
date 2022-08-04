63fb3490a7254f8e2426bd7c4f2820ca954ee0c9
sebin kwon <sbkwon98@gmail.com>
2022-08-04 22:53:07 +0900

    2회차 권세빈 0804 실습 추가

--- 2회차/권세빈/20220804/2_박스.py
+++ 2회차/권세빈/20220804/2_박스.py
@@ -4,15 +4,18 @@ import sys
 sys.stdin = open('2.txt', 'r')
 from pprint import pprint
 
+import sys
+input = sys.stdin.readline
+
 T = int(input())
 for t in range(T):
-    m, n = map(int,input().split())
-    box = [list(map(int,input().split())) for _ in range(5)]
-    col_box = []
+    n, m = map(int,input().split())
+    box = [list(map(int,input().split())) for _ in range(n)]
+    cnt = 0
 
-    for i in range(n):   
-        col = [box[j][i] for j in range(m)]
-        col_box.append(col)
-        for k in range(4,-1,-1):
-            print(col_box[i][k])
-
+    for i in range(m):
+        for j in range(n-1):
+            for k in range(j+1,n):
+                if box[j][i] > box[k][i]:
+                    cnt += 1
+    print(cnt)
