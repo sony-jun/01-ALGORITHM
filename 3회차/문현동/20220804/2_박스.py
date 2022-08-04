b6f5e496ebc51430ef2a41cfe1577ba3aef27556
mhd329 <mhd329@gmail.com>
2022-08-04 23:41:53 +0900

    1기 3회차 문현동 제출

--- 3회차/문현동/20220804/2_박스.py
+++ 3회차/문현동/20220804/2_박스.py
@@ -8,19 +8,5 @@ T = int(sys.stdin.readline())
 for test_case in range(T):
     n, m = map(int, sys.stdin.readline().split())
     cell = [[*map(int, sys.stdin.readline().split())]for _ in range(n)]
-    revrs = []
-    for c in range(m): # 4
-        tmp = []
-        for r in range(n): # 5
-            tmp.append(cell[r][c])
-        revrs.append(tmp)
-    res = 0
-    for r in revrs:
-        cnt = 0
-        for box in range(1, len(r)):
-            if r[box - 1] == 1 and r[box] == 0:
-                r[box - 1], r[box] = r[box], r[box - 1]
-                cnt += 1
-        res += cnt
-    print(revrs)
-    pprint(res)
+    
+    print(cell)
