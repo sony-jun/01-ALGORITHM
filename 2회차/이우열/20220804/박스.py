2ee5411e88b564262b3cb4619edbc82a958b2139
w00ye0l <dnduf158@gmail.com>
2022-08-05 10:39:42 +0900

    1기 2회차 이우열 20220804

--- 2회차/이우열/20220804/박스.py
+++ 2회차/이우열/20220804/박스.py
@@ -6,18 +6,11 @@ for _ in range(t):
     for i in range(n):
         box.append(list(map(int, input().split())))
 
-    re_box = []
-    for i in range(m):
-        temp = []
-        for j in range(n):
-            temp.append(box[j][i])
-        re_box.append(temp)
-
     move = 0
     for i in range(m):
         for j in range(n):
             for k in range(j + 1, n):
-                if re_box[i][j] == 1 and re_box[i][k] == 0:
+                if box[j][i] == 1 and box[k][i] == 0:
                     move += 1
 
     print(move)
