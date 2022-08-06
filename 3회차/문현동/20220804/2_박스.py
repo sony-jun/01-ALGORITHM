49565746c4f9dcdb4dc92a512dc9439a9ea2b065
mhd329 <mhd329@gmail.com>
2022-08-07 01:42:58 +0900

    1기 3회차 문현동 완성하여 제출

--- 3회차/문현동/20220804/2_박스.py
+++ 3회차/문현동/20220804/2_박스.py
@@ -1,12 +1,22 @@
 import sys
-from pprint import pprint
-
 sys.stdin = open("2_박스.txt", 'r')
 
 T = int(sys.stdin.readline())
 
 for test_case in range(T):
     n, m = map(int, sys.stdin.readline().split())
-    cell = [[*map(int, sys.stdin.readline().split())]for _ in range(n)]
+    cell = []
+    cnt = 0
     
-    print(cell)
+    for _ in range(n):
+        cell.append([*map(int, sys.stdin.readline().split())])
+    
+    for j in range(m):
+        for i in range(n - 1, -1, -1):
+            if cell[i][j] == 1:
+                while i + 1 != n and cell[i + 1][j] == 0:
+                    cell[i][j], cell[i + 1][j] = cell[i + 1][j], cell[i][j]
+                    i += 1
+                    cnt += 1
+                
+    print(cnt)
