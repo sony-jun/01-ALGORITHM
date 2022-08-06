70d7f67008c77cc55f29e64d1a2d1849aab8e9d5
djlfjfeq <leedc133@gmail.com>
2022-08-07 02:36:11 +0900

    0804

--- 1회차/이대찬/20220804/박스.py
+++ 1회차/이대찬/20220804/박스.py
@@ -1,4 +1,4 @@
-from doctest import master
+from dataclasses import asdict
 import sys
 
 sys.stdin = open('박스.txt')
@@ -7,24 +7,21 @@ N = int(input())
 
 for i in range(N):
     m,n = map(int,(input().split()))
-    matrix = [list((input().split())) for _ in range(m)]
+    matrix = [list(map(int,(input().split()))) for _ in range(m)]
     
     cnt =0
     for c in range(n):
         while(1):
             T = 0
             for r in range(1,m):
-            
                 if  matrix[r-1][c] ==1 and matrix[r][c] ==0:
                     matrix[r-1][c] =0 
                     matrix[r][c] =1
-                    print(matrix)
                     cnt +=1
                     T +=1
             if T == 0:
                 break    
-            
     print(cnt)
-            
+    
 
- 
+    
