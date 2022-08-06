0dd146944e01150bb1496f826ec1315434b4d846
MoonSanghee <psycho13th@gmail.com>
2022-08-06 20:27:17 +0900

    20220804 실습문제 지뢰찾기 풀이 추가

--- 2회차/문상희/20220804/76_직사각형네개의합집합의면적구하기.py
+++ 2회차/문상희/20220804/76_직사각형네개의합집합의면적구하기.py
@@ -14,27 +14,3 @@ cnt = 0
 for line in base:
     cnt += sum(line)
 print(cnt)
-
-
-
-
-# seet = []
-# for _ in range(4):
-#     line = list(map(int, input().split()))
-#     seet.append(line)
-
-# a1 = (seet[0][2] - seet[0][0]) * (seet[0][3] - seet[0][1])
-
-# a2 = a1 + ((seet[1][2] - seet[1][0]) * (seet[1][3] - seet[1][1])) - ((seet[1][0] - seet[0][2]) * (seet[1][1] - seet[0][1]))
-# # print(a1, a2)
-# a3 = a2 + ((seet[2][2] - seet[2][0]) * (seet[2][3] - seet[2][1]))
-
-# area = 0
-# for i in range(4):
-#     area += (seet[i][2] - seet[i][0]) * (seet[i][3] - seet[i][1])
-#     for j in range(i):
-#         if seet[j][2] - seet[i][0] > 0 and seet[j][3] - seet[i][1] > 0:
-#             area -= ((seet[j][2] - seet[i][0]) * (seet[j][3] - seet[i][1]))
-# print(area)
-
-
--- 2회차/문상희/20220804/78_지뢰찾기.py
+++ 2회차/문상희/20220804/78_지뢰찾기.py
@@ -1,21 +1,48 @@
+def cnt(i, j, seet, n):
+    di = [-1, -1, -1, 0, 0, 1, 1, 1]
+    dj = [-1, 0, 1, -1, 1, -1, 0, 1]
+    mine = 0
+    for _ in range(8):
+        ni = i + di[_]
+        nj = j + dj[_]
+        if 0 <= ni < n and 0 <= nj < n:
+            if seet[ni][nj] == '*':
+                mine += 1
+    return mine
+
 n = int(input())
-mine = []
-click = []
-for _ in range(n):
-    line = list(input())
-    mine.append(list)
+m_seet = []
+c_seet = []
 
 for _ in range(n):
-    line = list(input())
-    click.append(line)
+    sen = list(input())
+    m_seet.append(sen)
 
-open = []
+for _ in range(n):
+    sen = list(input())
+    c_seet.append(sen)
+
+mine_touch = 0
+open_seet = []
 for i in range(n):
+    line = []
     for j in range(n):
-        if click[i][j] == 'X':
-            if mine[i][j] =='*':
-                print('*', end ='')
-            else:
-                around = []
-                if i == 0 or j == 0:
-                    
+        if c_seet[i][j] == 'x':
+            line.append(cnt(i, j, m_seet, n))
+            if m_seet[i][j] == '*':
+                mine_touch = 1
+        else:
+            line.append('.')
+    open_seet.append(line)
+
+if mine_touch == 1:
+    for i in range(n):
+        for j in range(n):
+            if m_seet[i][j] == '*':
+                open_seet[i][j] = '*'
+
+for i in range(len(open_seet)):
+    for  j in open_seet[i]:
+        print(j, end = '')
+    print()
+
