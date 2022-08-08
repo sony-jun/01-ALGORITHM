ebadf6d83626181a06d8e90e7bc053e46d3f4ad4
sony-jun <warrise@naver.com>
2022-08-08 18:17:05 +0900

    8월8일코드

--- 1회차/손희준/22020804/04_back_jun_4396.py
+++ 1회차/손희준/22020804/04_back_jun_4396.py
@@ -6,7 +6,7 @@ for _ in range(num):
     for i in range(1,len(matrix)-1):
         for j in range(1,len(matrix)-1):
             if matrix[i][j] == '*':
-                bin[i][j] =='*'
+                
 
                 bin[i][j-1] +=1
                 bin[i][j+1] +=1
