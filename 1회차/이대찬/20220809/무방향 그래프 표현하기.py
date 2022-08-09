5b862cb342a1cdb3cbd6f30b4e23ee2d32113d7a
djlfjfeq <leedc133@gmail.com>
2022-08-09 23:53:29 +0900

    0809

---
+++ 1회차/이대찬/20220809/무방향 그래프 표현하기.py
@@ -0,0 +1,27 @@
+import sys
+
+sys.stdin = open('무방향 그래프 표현하기.txt')
+
+n,m = map(int,input().split())
+result_matrix = [[0]*(n+1) for _ in range(n+1)]
+
+result_list = [[] for _ in range(n+1)]
+
+for i in range(m):
+    u,v = map(int,input().split())
+    result_list[u].append(v)
+    result_list[v].append(u)
+    
+    result_matrix[u][v] = 1
+    result_matrix[v][u] = 1
+    
+print(result_matrix)    
+print(result_list)
+
+    
+    
+    
+ 
+ 
+
+ 
