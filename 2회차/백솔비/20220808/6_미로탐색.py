dc81db20d652d6ffd847fb5b937942a8f77c92ea
hae-on <solbi2004@naver.com>
2022-08-09 16:41:50 +0900

    0808 실습 풀이 덜한 부분

--- 2회차/백솔비/20220808/6_미로탐색.py
+++ 2회차/백솔비/20220808/6_미로탐색.py
@@ -1,3 +1,28 @@
 import sys
 sys.stdin = open("6_미로탐색.txt")
+from collections import deque
 
+N, M = map(int, input().split())
+board = [list(map(int,input())) for _ in range(N)]
+
+dx = [-1, 1, 0, 0] 
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 1:
+                queue.append((nx, ny))
+                board[nx][ny] = board[x][y]+1
+    
+    return board[N-1][M-1]
+
+print(bfs(0,0))
