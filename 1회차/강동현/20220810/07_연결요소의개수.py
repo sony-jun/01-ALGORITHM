f7b990c97f982fe14d9c58b732f12a790344582c
kangdh208 <kangdh208@gmail.com>
2022-08-10 16:17:04 +0900

    0810 연습문제 풀이

---
+++ 1회차/강동현/20220810/07_연결요소의개수.py
@@ -0,0 +1,110 @@
+# 방향 없는 그래프가 주어졌을 때, 연결 요소 (Connected Component)의 개수를 구하는 프로그램을 작성하시오.
+
+
+# dfs --- 시간초과
+# dfs로 그래프를 탐색한다.
+import sys
+
+sys.setrecursionlimit(10000)
+def dfs(start, depth):
+
+    #해당 노드 방문체크 한다.
+    visited[start] = True
+
+    # 해당 시작점을 기준으로 계속해서 dfs로 그래프를탐색한다.
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(i, depth + 1)
+
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 방문처리
+visited = [False] * (1 + N)
+count = 0  # 컴포넌트 그래프 개수 저장
+
+# 1~N번 노드를 각각돌면서
+for i in range(1, N + 1):
+    if not visited[i]:  # 만약 i번째 노드를 방문하지 않았다면
+        if not graph[i]:  # 만약 해당 정점이 연결된 그래프가 없다면
+            count += 1  # 개수를 + 1
+            visited[i] = True  # 방문 처리
+        else:  # 연결된 그래프가 있다면
+            dfs(i, 0)  # dfs탐색을 돈다.
+            count += 1  # 개수를 +1
+
+print(count)
+
+# bfs
+from collections import deque
+
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        node = queue.popleft()
+        for i in graph[node]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 방문처리
+visited = [False] * (1 + N)
+count = 0  # 컴포넌트 그래프 개수 저장
+
+# 1~N번 노드를 각각돌면서
+for i in range(1, N + 1):
+    if not visited[i]:  # 만약 방문하지 않았다면
+        if not graph[i]:  # 만약 그래프가 비어있다면
+            count += 1  # 개수 1개 추가
+            visited[i] = True  # 방문 처리
+        else:  # 만약 그래프가 비어있지 않다면(어느 점과 연결된 점이 있다면)
+            bfs(i)  # 해당 i를 시작노드로 bfs를 돈다.
+            count += 1  # 연결요소 를 +1개 해준다.
+
+print(count)
+
+# 긁풀이
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+N, M = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(N+1)]
+graph[0] = [0,0]
+visited = [False for _ in range(N+1)]
+cnt = 0
+
+for _ in range(M):
+    start, end = map(int, input().split())
+    graph[start].append(end)
+    graph[end].append(start)
+    graph[start].sort()
+    graph[end].sort()
+
+def DFS(graph, start, visited):
+    visited[start] = True
+    for i in graph[start]:
+        if not visited[i]:
+            DFS(graph, i, visited)
+for i in range(1, len(visited)):
+    if visited[i] == False:
+        cnt += 1
+        DFS(graph, i, visited)
+print(cnt)
