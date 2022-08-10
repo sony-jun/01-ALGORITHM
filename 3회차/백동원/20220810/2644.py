e13d6db090ccbbc697c6376f38d4292640641c25
dongwonbaek <bmrio100@gmail.com>
2022-08-10 21:38:44 +0900

    실습문제 풀이 추가

--- 3회차/백동원/20220810/2644.py
+++ 3회차/백동원/20220810/2644.py
@@ -1,9 +1,24 @@
 # 촌수계산 미해결
-def dfs(node):
-    for n in matrix[node]:
-        if visited[n] == 0:
-            visited[n] = visited[node]+1
-            dfs(n)
+# def dfs(node):
+#     for n in matrix[node]:
+#         if visited[n] == 0:
+#             visited[n] = visited[node]+1
+#             dfs(n)
+# n = int(input())
+# u, v = map(int, input().split())
+# m = int(input())
+# matrix = [[] for _ in range(n + 1)]
+# visited = [0] * (n + 1)
+# for _ in range(m):
+#     x, y = map(int, input().split())
+#     matrix[x].append(y)
+#     matrix[y].append(x)
+# dfs(u)
+# if visited[v] > 0:
+#     print(visited[v])
+# else:
+#     print(-1)  
+
 n = int(input())
 u, v = map(int, input().split())
 m = int(input())
@@ -13,8 +28,14 @@ for _ in range(m):
     x, y = map(int, input().split())
     matrix[x].append(y)
     matrix[y].append(x)
-dfs(u)
+stack = [u]
+while len(stack) != 0:
+    cur = stack.pop()
+    for a in matrix[cur]:
+        if visited[a] == 0:
+            visited[a] = visited[cur] + 1
+            stack.append(a)
 if visited[v] > 0:
     print(visited[v])
 else:
-    print(-1)  
+    print(-1)
