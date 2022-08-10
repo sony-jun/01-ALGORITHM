15561e4c28418cdd5f584895bbca013ed8f6dd5d
mhd329 <mhd329@gmail.com>
2022-08-11 06:11:03 +0900

    1기 3회차 문현동 제출

--- 3회차/문현동/20220810/1_촌수계산.py
+++ 3회차/문현동/20220810/1_촌수계산.py
@@ -1,5 +1,4 @@
 import sys
-from pprint import pprint
 sys.stdin = open("1_촌수계산.txt", 'r')
 
 relation = []
@@ -22,26 +21,17 @@ for i in relation:
     adjacent_list[i[1]].append(i[0])
 
 visited = [0] * (N + 1)
-visited[0] = 1
 
 cnt = 0
-run = True
 stack = [r1]
+run = True
+
+print(adjacent_list)
+
 while stack and run: # r1 에 대한 while 문
     current = stack.pop(0)
     for j in adjacent_list[current]:
         if not visited[j]:
-            if j == r2: # 3
-                visited[j] = 1
-                run = False
-                break
-            else:
-                visited[j] = 1 
-                stack.append(j)
-    cnt += 1
-# r1 을 다 돌렸음에도 visited 안에 0 이 있다면 -1 출력
-print(visited)
-if run:
-    print(-1)
-else:
-    print(cnt)
+            cnt += 1
+            visited[j] = 1 
+            stack.append(j)
