9408293ba6f4be0dcb05a455dac233f167e9b123
YoonDii <dbswl9506@naver.com>
2022-08-10 21:01:16 +0900

    0810김윤지

---
+++ 1회차/김윤지/20220810/11724.py
@@ -0,0 +1,12 @@
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+
