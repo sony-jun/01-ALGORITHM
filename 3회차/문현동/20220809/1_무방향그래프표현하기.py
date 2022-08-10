5c2f765ae133b01fffbb6244265ccad993d98ad3
mhd329 <mhd329@gmail.com>
2022-08-10 19:31:24 +0900

    2022 08 09 1기 3회차 문현동

---
+++ 3회차/문현동/20220809/1_무방향그래프표현하기.py
@@ -0,0 +1,28 @@
+import sys
+from pprint import pprint
+sys.stdin = open("1_무방향그래프표현하기.txt", 'r')
+
+N, M = map(int, sys.stdin.readline().split())
+
+adjacent_list = []
+adjacent_matrix = []
+
+for _ in range(N + 1):
+    adjacent_matrix.append([0] * (N + 1))
+    adjacent_list.append([])
+
+edges = []
+
+for _ in range(M):
+    edges.append([*map(int, sys.stdin.readline().split())])
+
+for v1, v2 in edges:
+    adjacent_matrix[v1][v2] = 1
+    adjacent_matrix[v2][v1] = 1
+
+for i in edges:
+    adjacent_list[i[0]].append(i[1]) # 인접 리스트의 i[0] 번째 공간에 i[1] 의 값을 추가
+    adjacent_list[i[1]].append(i[0]) # 인접 리스트의 i[1] 번째 공간에 i[0] 의 값을 추가
+
+pprint(adjacent_list)
+pprint(adjacent_matrix)
