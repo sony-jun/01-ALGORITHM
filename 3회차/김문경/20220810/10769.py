7b0aa100d8f1672ca671fb878a78ce78a938f68b
kmk4162 <kmk41623@gmail.com>
2022-08-11 00:08:07 +0900

    Update Algorithm 220810

---
+++ 3회차/김문경/20220810/10769.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+# sys.stdin = open('test.txt')
+
+words = input()
+
+happy = words.count(':-)')
+sad = words.count(':-(')
+
+if happy == sad:
+    if happy == 0 and sad == 0:
+        print('none')
+    else:
+        print('unsure')
+elif happy > sad:
+    print('happy')
+elif happy < sad:
+    print('sad')
---
+++ 3회차/김문경/20220810/4963.py
@@ -0,0 +1,5 @@
+import sys
+input = sys.stdin.readline
+sys.stdin = open('test.txt')
+
+
