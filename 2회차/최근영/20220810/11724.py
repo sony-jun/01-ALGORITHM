0a8674f8c53044088928fdadde522c04dc23f95c
choikeunyoung <ought119@gmail.com>
2022-08-11 23:41:13 +0900

    20220811

--- 2회차/최근영/20220810/11724.py
+++ 2회차/최근영/20220810/11724.py
@@ -6,11 +6,16 @@ for i in range(m):
     x, y = map(int,input().split())
     matrix[x].append(y)
     matrix[y].append(x)
-
+total_list=[]
+pprint(matrix)
 for j in range(1,len(matrix)):
     stack = list(matrix[j])
     visit = [False]*(n+1)
     visit[j] = True
     while stack:
-        
-    
+        cur = stack.pop()
+        for k in matrix[cur]:
+            if visit[k] == False:
+                visit[k] = True
+                stack.append(k)
+    print(visit)
