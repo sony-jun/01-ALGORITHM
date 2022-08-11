29eb608b575b258be21a820a54c9eb8c05c1bbda
danny7128 <danny7128@gmail.com>
2022-08-11 13:48:55 +0900

    1기_3회차_이성훈_20220810_필수 문제 완료

---
+++ 3회차/이성훈/20220810/0_바이러스.py
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/2606
+import sys
+
+sys.stdin = open("0_바이러스.txt")
+
+
+    
---
+++ 3회차/이성훈/20220810/1_연결 요소의 개수.py
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/11724
+import sys
+
+sys.stdin = open("1_연결 요소의 개수.txt")
+
+
+    
