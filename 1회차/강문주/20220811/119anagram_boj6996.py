951f181844946ccb78877d796fb0e9270d03c3c2
mororong9 <kangmoonju0603@gmail.com>
2022-08-11 17:33:58 +0900

    algorithm

--- 1회차/강문주/20220811/119anagram_boj6996.py
+++ 1회차/강문주/20220811/119anagram_boj6996.py
@@ -7,9 +7,10 @@ T = int(input())
 for i in range(T):
     a, b = map(str,input().split())
     x = sorted(a)
-    y = sorted(b)
+    y = sorted(b) #정렬해서 
 
     if x == y:
-        print(a, "&", b, "are anagrams.")
+        print(a, "&", b, "are anagrams.") #일치하면 anagram
     else:
         print(a, "&", b, "are NOT anagrams.")
+        #a순서 바꿔서 b만들 수 있으면 anangram
