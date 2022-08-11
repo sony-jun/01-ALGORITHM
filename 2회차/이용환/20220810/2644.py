4e638f8c8d282955361a6ba70b6f651e97dc3a38
tnpfldyd <dkdldhs0322@gmail.com>
2022-08-11 18:09:53 +0900

    0810

--- 2회차/이용환/20220810/2644.py
+++ 2회차/이용환/20220810/2644.py
@@ -7,16 +7,29 @@ for i in range(R):
     num1, num2 = map(int, input().split())
     matrix[num2].append(num1)
     matrix[num1].append(num2)
-start = [a]
-visited = [0] * T
+# start = [a]
+# visited = [0] * T
+# while start:
+#     chon = start.pop()
+#     if chon == b:
+#         print(visited[b])
+#         break
+#     for i in matrix[chon]:
+#         if visited[i] == 0:
+#             visited[i] = visited[chon] + 1
+#             start.append(i)
+# else:
+#     print(-1)
+visited = [False] * T
+start = [(a, 0)]
+answer = -1
 while start:
-    chon = start.pop()
+    chon, cnt = start.pop()
     if chon == b:
-        print(visited[b])
+        answer = cnt
         break
-    for i in matrix[chon]:
-        if visited[i] == 0:
-            visited[i] = visited[chon] + 1
-            start.append(i)
-else:
-    print(-1)
+    for adj in matrix[chon]:
+        if not visited[adj]:
+            visited[adj] = True
+            start.append((adj, cnt + 1))
+print(answer)
