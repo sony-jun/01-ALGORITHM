72626c93d49760c28ac5167d2c1baa002562ddcc
hvvany <kjunhwan1998@gmail.com>
2022-08-11 22:12:07 +0900

    fix 0810

--- 3회차/김준환/20220810/2644-촌수.py
+++ 3회차/김준환/20220810/2644-촌수.py
@@ -1,17 +1,39 @@
-from pprint import pprint
+import sys
+input = sys.stdin.readline
 
-n, m = map(int,input().split())
-matrix = [[0]*(n+1) for _ in range(n+1)]
+# 비어있는 인접 행렬, 방문 체크 리스트 만들기
+n = int(input())
+start, end = map(int,input().split())
+m = int(input())
+
+graph = [[] for _ in range(n+1)]
+visited = [-1]*(n+1)
+
+# 인접 행렬 완성
 for _ in range(m):
-    x, y = map(int,input().split())
-    matrix[x][y] = 1
-pprint(matrix)
-lst = []
-print(lst)
-for x in range(n+1):
-    lst2 = []
-    for y in range(n+1):
-        if matrix[x][y] == 1:
-            lst2.append(y)
-    lst.append(lst2)
-print(lst)
+    v1, v2 = map(int,input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+# dfs 함수 활용
+def Dfs(start):
+    stack = [start]
+    visited[start] = 0
+    cnt = 1
+
+    while stack:
+        cur = stack.pop()
+
+        for adj in graph[cur]:
+            if adj == end:
+                cnt += 1
+                visited[adj] += cnt
+                return
+            if visited[adj] == -1:
+                visited[adj] += cnt
+                stack.append(adj)
+                if adj == graph[cur][-1]:
+                    cnt += 1
+        
+Dfs(start)
+print(visited[end])
