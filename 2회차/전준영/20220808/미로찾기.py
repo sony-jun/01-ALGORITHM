110e5559ed6cb02c6097c9138178fa38a513bc93
slugjeon <youngman0515@naver.com>
2022-08-16 09:18:11 +0900

    08/15

--- 2회차/전준영/20220808/미로찾기.py
+++ 2회차/전준영/20220808/미로찾기.py
@@ -1,18 +1,11 @@
 import sys
+from copy import deepcopy
 from collections import deque
 input=sys.stdin.readline
 class coordinate:
-    def __init__(self,idx,arr,N,M,cnt):
-        self.idx=[]
-        self.idx.append(idx[0])
-        self.idx.append(idx[1])
-        self.arr=[]
-        for i in arr:
-            ar0=[]
-            for k in i:
-                ar0.append(k)
-            self.arr.append(ar0)
-        self.cnt=cnt
+    def __init__(self,idx):
+        self.idx=deepcopy(idx)
+        self.cnt=1
 vector=[[1,0],[0,1],[-1,0],[0,-1]]
 N,M=map(int,input().split())
 arr=[]
@@ -23,26 +16,30 @@ for _ in range(N):
         if(i!='\n'):
             ttt.append(int(i))
     arr.append(ttt)
-stack=[]
-stack.append(coordinate([0,0],arr,N,M,1))
-temp1=[]
-t=0
-cnt=0
+queue=deque()
 min=M*N
-while stack:
-    temp=stack.pop()
-    temp.arr[temp.idx[0]][temp.idx[1]]=0
-    bo=0
+cnt=1
+temp2=[0,0]
+queue.append(coordinate([0,0]))
+cnt=1
+visited=[[False]*M for _ in range(N)]
+visited[0][0]=True
+t=0
+while t!=1:
+    temp=queue.popleft()
     for i in vector:
-        temp1.clear()
-        temp1.append(temp.idx[0]+i[0])
-        temp1.append(temp.idx[1]+i[1])
-        if(0<=temp1[0]<N and 0<=temp1[1]<M and temp.arr[temp1[0]][temp1[1]]==1):
-            temp.cnt+=1
-            if(temp1[0]==N-1 and temp1[1]==M-1):
-                if(min>temp.cnt):
-                    min=temp.cnt
+        temp1=deepcopy(temp)
+        temp2[0]=temp1.idx[0]+i[0]
+        temp2[1]=temp1.idx[1]+i[1]
+        if(0<=temp2[0]<N and 0<=temp2[1]<M and arr[temp2[0]][temp2[1]] and not visited[temp2[0]][temp2[1]]):
+            if(temp2[0]==N-1 and temp2[1]==M-1):
+                t=1
+                min=temp1.cnt+1
+                break
             else:
-                stack.append(coordinate(temp1,temp.arr,N,M,temp.cnt))
-            bo+=1
+                temp1.idx[0]=temp2[0]
+                temp1.idx[1]=temp2[1]
+                temp1.cnt+=1
+                visited[temp2[0]][temp2[1]]=True
+                queue.append(temp1)
 print(min)
