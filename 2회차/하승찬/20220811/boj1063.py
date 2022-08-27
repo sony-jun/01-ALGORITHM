e7af363c8cacc3554cc6850bc9e1f6847f9215ae
caretim <karesin72041@gmail.com>
2022-08-27 13:46:37 +0900

    20220827

--- 2회차/하승찬/20220811/boj1063.py
+++ 2회차/하승찬/20220811/boj1063.py
@@ -1,15 +1,14 @@
 
 n=8
 
-st,king,sq = input().split()
+king,st,sq = input().split()
 
 
-chess = [[0]*n for __ in range(n)]
 
 move = {
-    'TR': ( 1,1),
+    'RT': (1,1),
     'T':  (1,0),
-    'TL': (1,-1),
+    'LT': (1,-1),
     'L': (0,-1),
     'R':(0,1),
     'LB':(-1,-1),
@@ -17,42 +16,95 @@ move = {
     'RB':(-1,1)
 }
 
-
-sx= (ord(st[0])-65)
-sy= 7-(int(st[1])-1)
 kx= (ord(king[0])-65)
-ky= 7-(int(king[1])-1)
-
-print(sx,sy,kx,ky)
-chess[sy][sx]=2
-chess[ky][kx]=1
+ky= (int(king[1])-1)
+sx= (ord(st[0])-65)
+sy= (int(st[1])-1)
 
 
-# def cm (y,x):
-#     if y
+
+
+
 for p in range(int(sq)):
 
-    #moving =input()
-    mo=move[input()]
-
-    smy = sy + mo[0] 
-    smx = sx + mo[1]
-    if 0>smy or smy >= n or 0>smx or smx >=n:
-        pass
-    else :
-        sy=smy
-        sx=smx
-
+    mo=move.get(input())
     kmy = ky + mo[0]
     kmx = kx + mo[1]
-    if 0>kmy or kmy >= n or 0>kmx or kmx >=n:
-        pass
-    else :
-        ky=kmy
-        kx=kmx
+    
+    if 0>kmy or kmy >= 8 or 0>kmx or kmx >=8:
+        continue
+    elif 0<=kmy or kmy < 8 or 0<=kmx or kmx <8:
+            if kmy == sy and kmx == sx:
+                smy = sy + mo[0] 
+                smx = sx + mo[1]
+                if 0>smy or smy >= n or 0>smx or smx >=n:
+                    continue
+                elif 0<=smy or smy < 8 or 0<=smx or smx <8:
+                    sy = smy
+                    sx = smx 
+                    ky =kmy
+                    kx =kmx
+            else:
+                ky =kmy
+                kx =kmx
 
-# kx=kx +65 
-print (chr(sx+65),sy)
-print (chr(kx+65),ky)
+print (chr(kx+65),ky+1,sep='')
+print (chr(sx+65),sy+1,sep='')
 
-# sx =sx+65
+#
+n=8
+
+king,st,sq = input().split()
+
+
+
+move = {
+    'RT': (1,1),
+    'T':  (1,0),
+    'LT': (1,-1),
+    'L': (0,-1),
+    'R':(0,1),
+    'LB':(-1,-1),
+    'B':(-1,0),
+    'RB':(-1,1)
+}
+
+kx= (ord(king[0])-65)
+ky= (int(king[1])-1)
+sx= (ord(st[0])-65)
+sy= (int(st[1])-1)
+
+
+
+
+
+for p in range(int(sq)):
+
+    mo=move.get(input())
+    kmy = ky + mo[0]
+    kmx = kx + mo[1]
+    
+    if 0>kmy or kmy >= 8 or 0>kmx or kmx >=8:
+        continue
+    elif 0<=kmy or kmy < 8 or 0<=kmx or kmx <8:
+            if kmy == sy and kmx == sx:
+                smy = sy + mo[0] 
+                smx = sx + mo[1]
+                if 0>smy or smy >= n or 0>smx or smx >=n:
+                    continue
+                elif 0<=smy or smy < 8 or 0<=smx or smx <8:
+                    sy = smy
+                    sx = smx 
+                    ky =kmy
+                    kx =kmx
+            else:
+                ky =kmy
+                kx =kmx
+
+print (chr(kx+65),ky+1,sep='')
+print (chr(sx+65),sy+1,sep='')
+
+
+
+
+
